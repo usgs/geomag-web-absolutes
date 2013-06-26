b6ec46f253a6436240274edcd817c892c51f71a5
Jeremy Fee <jmfee@usgs.gov>
2013-06-26 16:52:18 -0600

    Implemented Observatory getDefaultPier, added unit tests

--- test/spec/ObservatoryTest.js
+++ test/spec/ObservatoryTest.js
@@ -5,17 +5,49 @@
 define([
 	'chai',
 	'geomag/Observatory',
-	'spec/ObservatoryFeed'
-], function (chai, Observatory, ObservatoryFeed) {
+	'mvc/Collection',
+	'mvc/Util'
+], function (chai, Observatory, Collection, Util) {
 	'use strict';
 	var expect = chai.expect;
+
+
+	var TEST_OBSERVATORY_DATA = {
+		'id': 'example_obs_001',
+		'piers': new Collection([
+			{
+				'id': 'test_pier_1'
+			},
+			{
+				'id': 'test_pier_2'
+			}
+		]),
+		'default_pier_id': null
+	};
+
 
 	describe('Unit tests for the "Observatory" class', function () {
 
 		describe('constructor()', function () {
-			it('id attribute matches test data', function () {
-				var observatory = new Observatory(ObservatoryFeed.observatories[0]);
+			it('calls Model constructor', function () {
+				var observatory = new Observatory(TEST_OBSERVATORY_DATA);
 				expect(observatory.get('id')).to.equal('example_obs_001');
+			});
+		});
+
+		describe('getDefaultPier()', function () {
+			it('returns the default pier when specified', function () {
+				var observatory = new Observatory(
+						Util.extend({}, TEST_OBSERVATORY_DATA,
+								{'default_pier_id': 'test_pier_1'}));
+				var defaultPier = observatory.getDefaultPier();
+				expect(defaultPier.id).to.equal('test_pier_1');
+			});
+
+			it('returns null when no default specified', function () {
+				var observatory = new Observatory(TEST_OBSERVATORY_DATA);
+				var defaultPier = observatory.getDefaultPier();
+				expect(defaultPier).to.equal(null);
 			});
 		});
 
