3ba3adefa81d0fdd9cd799b72994ae6276e18c1c
Edward McWhirter Jr <emcwhirter@usgs.gov>
2013-06-27 09:53:50 -0600

    Re-added Pier class with getDefaultMark

---
+++ src/htdocs/js/geomag/Pier.js
@@ -0,0 +1,62 @@
+
+/*global define*/
+
+define([
+	'mvc/Model',
+	'mvc/Util',
+	'mvc/Collection'
+], function(
+	Model,
+	Util,
+	Collection
+) {
+	'use strict';
+
+
+	/** Define default attributes. */
+	var DEFAULTS = {
+		'id': null,
+		'pier_name': null,
+		'begin': null,
+		'correction': null,
+		'default_mark_id': null,
+		'default_electronics_id': null,
+		'default_theodolite_id': null,
+		'marks': null
+	};
+
+  
+	/**
+	 * Constructor.
+	 *
+	 * @param  options {Object} pier attributes.
+	 */
+	var Pier = function (options) {
+		// Call parent constructor
+		Model.call(this, Util.extend({}, DEFAULTS, options));
+
+	};
+
+	// Pier extends Model
+	Pier.prototype = Object.create(Model.prototype);
+
+	
+	/**
+	 * Get the default mark for the pier.
+	 *
+	 * @return {Mark} the default mark, or null if no default is specified.
+	 */
+	 Pier.prototype.getDefaultMark = funtion () {
+		var marks = this.get('marks'),
+			default_mark_id = this.get('default_mark_id');
+		if (marks !== null && default_mark_id !== null) {
+			return marks.get(default_mark_id);
+		} else {
+			return null;
+		}
+	 };
+
+
+	// return constructor from closure
+	return Pier;
+});
