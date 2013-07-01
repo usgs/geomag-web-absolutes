de5491f78af2f7245b3caf68d6e919dcfcf1299d
Hal Simpson <hasimpson@usgs.gov>
2013-07-01 13:28:56 -0600

    Changed test to use expect instanceOf

--- test/spec/MarkTest.js
+++ test/spec/MarkTest.js
@@ -11,17 +11,18 @@ define([
 	var expect = chai.expect;
 
 	describe('Unit tests for the "Mark" class', function () {
-		var testObect = { name:  'Main',
-											begin: 1372193224820,
-											end:   1372193224880,
-											azimuth: 176.1
+		var testObect = {
+			'name': 'Main',
+			'begin': 1372193224820,
+			'end': 1372193224880,
+			'azimuth': 176.1
 		};
 
 		describe('constructor()', function () {
 			it('evaluates to instance of "Mark"', function () {
 				var mark = new Mark();
-				expect(mark instanceof Mark);
-				expect(mark instanceof Model);
+				expect(mark).to.be.an.instanceOf(Mark);
+				expect(mark).to.be.an.instanceOf(Mark);
 			});
 
 			it('works when passed an obect', function () {
