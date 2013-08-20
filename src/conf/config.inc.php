ab1bfcaf3611e828c5f7df0ad6489a67076a99cf
Jeremy Fee <jmfee@usgs.gov>
2013-08-20 12:06:21 -0600

    Removed binary sqlite database, removed reference to binary sqlite database from pre-install, updated config.inc.php to create schema when it is missing

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -11,5 +11,20 @@ $DATA_DIR = $CONFIG['DATA_DIR'];
 $MOUNT_PATH = $CONFIG['MOUNT_PATH'];
 
 
+$dburl = 'sqlite:' . $DATA_DIR . DIRECTORY_SEPARATOR . $CONFIG['SQLITE_DB'];
+$dbschema = '../lib/sql/sqlite/create_tables.sql';
+
+
 // connect to database
-$DB = new PDO('sqlite:' . $DATA_DIR . DIRECTORY_SEPARATOR . $CONFIG['SQLITE_DB']);
+$DB = new PDO($dburl);
+// use php exceptions for query problems
+$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// verify database has schema
+try {
+	$rs = $DB->query('select count(*) from observatory');
+} catch (PDOException $e) {
+	// database doesn't have schema, load
+	$DB->exec(file_get_contents($dbschema));
+}
+
