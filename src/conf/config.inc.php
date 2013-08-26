b0c441efe4e703cdb847250cf3dfedb561df3cbd
U-GS\emmerichc <emmerichc@IGSKBACBLT141a.gs.doi.net>
2013-08-26 17:02:34 -0600

    PHP Data Feed for Reference Data

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -12,13 +12,32 @@ $MOUNT_PATH = $CONFIG['MOUNT_PATH'];
 
 
 $dburl = 'sqlite:' . $DATA_DIR . DIRECTORY_SEPARATOR . $CONFIG['SQLITE_DB'];
-$dbschema = '../lib/sql/sqlite/create_tables.sql';
+$dbschema = $DATA_DIR . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 
+		DIRECTORY_SEPARATOR . 'sql' . DIRECTORY_SEPARATOR . 'sqlite' . 
+		DIRECTORY_SEPARATOR . 'create_tables.sql';
 
+// PHP Classes
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'Instrument.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR .  'Mark.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'Observatory.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'Observation.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'ObservatoryDetail.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'ObservatoryFactory.class.php';
+include_once $DATA_DIR . DIRECTORY_SEPARATOR .'lib' . DIRECTORY_SEPARATOR . 
+		'classes' . DIRECTORY_SEPARATOR . 'Pier.class.php';
 
 // connect to database
 $DB = new PDO($dburl);
 // use php exceptions for query problems
 $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$OBSERVATORY_FACTORY = new ObservatoryFactory($DB);
 
 // verify database has schema
 try {
@@ -27,4 +46,4 @@ try {
 	// database doesn't have schema, load
 	$DB->exec(file_get_contents($dbschema));
 }
-
+?>
---
+++ src/lib/classes/Mark.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Data class representing a mark.
+ */
+class Mark {
+	
+	// mark attributes
+	public $id;
+	public $pierId;
+	public $name;
+	public $begin;
+	public $end;
+	public $azimuth;
+	
+	public function __construct($id=null, $pierId=null, $name=null, 
+			                    $begin=null, $end=null, $azimuth=null) { 
+		$this->id = $id;
+		$this->pierId = $pierId;
+		$this->name = $name;
+		$this->begin = $begin;
+		$this->end = $end;
+		$this->azimuth = $azimuth;
+	}
+	
+	public static function fromArray(&$p) {
+		
+		return new Mark ($p['id'], $p['pierId'], $p['name'], $p['begin'], 
+				         $p['end'], $p['azimuth']);
+	}
+	
+	public function toArray() {
+		return array ('id' => $this->id, 
+				      'pierId' => $this->pierId,
+				      'name' => $this->name, 'begin' => $this->begin, 
+				      'end' => $this->end, 'azimuth' => $this->azimuth); 
+	}
+	
+	
+}
+
+
+?>
