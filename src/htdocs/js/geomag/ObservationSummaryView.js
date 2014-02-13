1c01a82d5901494ea6e6499c29baec6aab0913f7
Jonathan Brown <jdbrown@usgs.gov>
2014-02-13 16:27:25 -0700

    set up declination view rows

--- src/htdocs/js/geomag/ObservationSummaryView.js
+++ src/htdocs/js/geomag/ObservationSummaryView.js
@@ -1,10 +1,14 @@
 /* global define */
 define([
 	'mvc/View',
-	'util/Util'
+	'util/Util',
+
+	'geomag/DeclinationSummaryView'
 ], function (
 	View,
-	Util
+	Util,
+
+	DeclinationSummaryView
 ) {
 	'use strict';
 
@@ -23,58 +27,77 @@ define([
 
 
 	ObservationSummaryView.prototype.render = function () {
-		// TODO :: Render current model
+		var readings = this._readings.data(),
+				declinationSummaryView = this._declinationSummaryView,
+				calculator = this._calculator,
+				i = null,
+				len = null,
+				reading;
+		Util.empty(declinationSummaryView);
+		for (i = 0, len = readings.length; i < len; i++) {
+			reading = readings[i];
+			// create view if it does not exist
+			if (!reading.hasOwnProperty('_declinationSummary')) {
+				reading._declinationSummary = new DeclinationSummaryView({
+					el:document.createElement('tr'),
+					reading:reading,
+					calculator:calculator
+				});
+			}
+			// insert view
+			declinationSummaryView.appendChild(reading._declinationSummary._el);
+		}
 	};
 
 	ObservationSummaryView.prototype._initialize = function () {
+		var el = this._el;
+
 		this._observation = this._options.observation;
 		this._calculator = this._options.calculator;
 
 
+		this._readings = this._observation.get('readings');
 
 
-
-		this._el.innerHTML = [
+		el.innerHTML = [
 			'<section class="observation-summary-view">',
-				'<section class="row">',
 					'<h4>Declination</h4>',
-					'<section class="column one-of-two declination">',
-						'<table>',
-							'<thead>',
-								'<tr>',
-									'<th scope="col">&nbsp;</th>',
-									'<th scope="col" class="dec-absolute-sTime">Start Time</th>',
-									'<th scope="col" class="dec-absolute-eTime">End Time</th>',
-									'<th scope="col" class="dec-absolute-deg">Deg.</th>',
-									'<th scope="col" class="dec-absolute-min">Min.</th>',
-									'<th scope="col" class="dec-absolute-ordMin">Ord. Min.</th>',
-									'<th scope="col" class="dec-baseline-min">Min.</th>',
-									'<th scope="col" class="dec-baseline-nT">nT</th>',
-									'<th scope="col" class="dec-baseline-observer>Observer</th>',
-									'<th scope="col" class="dec-baseline-180-shift">180&#176; Shift</th>',
-								'</tr>',
-							'</thead>',
-							'<tbody class="declination-reading">',
-							'</tbody>',
-						'</table>',
-						'<table>',
-							'<tbody>',
-								'<tr>',
-									'<td class="declination-mean">Mean:</td>',
-									'<td class="declination-mean-outPut"></td>',
-								'</tr>',
-								'<tr>',
-									'<td class="declination-range">Range:</td>',
-									'<td class="declination-range-outPut"></td>',
-								'</tr>',
-								'<tr>',
-									'<td class="declination-stdDeviation">Std. Deviation:</td>',
-									'<td class="declination-stdDeviation"></td>',
-								'</tr>',
-							'</tbody>',
-						'</table>',
-					'</section>',
-				'</section>',
+					'<table>',
+						'<thead>',
+							'<tr>',
+								'<th scope="col" class="name">Set</th>',
+								'<th scope="col" class="valid">Valid</th>',
+								'<th scope="col" class="start-time">Start Time</th>',
+								'<th scope="col" class="end-time">End Time</th>',
+								'<th scope="col" class="degrees">Deg.</th>',
+								'<th scope="col" class="minutes">Min.</th>',
+								'<th scope="col" class="ord-min">Ord. Min.</th>',
+								'<th scope="col" class="baseline-min">Min.</th>',
+								'<th scope="col" class="baseline-nt">nT</th>',
+								'<th scope="col" class="observer">Observer</th>',
+								'<th scope="col" class="shift">180&#176; Shift</th>',
+							'</tr>',
+						'</thead>',
+						'<tbody class="declination-summary-view">',
+						'</tbody>',
+					'</table>',
+					'<table>',
+						'<tbody>',
+							'<tr>',
+								'<td class="declination-mean">Mean:</td>',
+								'<td class="declination-mean-outPut"></td>',
+							'</tr>',
+							'<tr>',
+								'<td class="declination-range">Range:</td>',
+								'<td class="declination-range-outPut"></td>',
+							'</tr>',
+							'<tr>',
+								'<td class="declination-stdDeviation">Std. Deviation:</td>',
+								'<td class="declination-stdDeviation"></td>',
+							'</tr>',
+						'</tbody>',
+					'</table>',
+
 				'<section class="row">',
 					'<h4>Horizontal Intensity</h4>',
 					'<section class="column one-of-two horizontalIntensity">',
@@ -183,6 +206,9 @@ define([
 					'</section>',
 			'</section>'
 		].join('');
+
+		this._declinationSummaryView = el.querySelector('.declination-summary-view');
+		this.render();
 	};
 
 
