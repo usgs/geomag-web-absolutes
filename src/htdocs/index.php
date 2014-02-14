57da56ffa259f4ce68e725cde3175b9d65402fcf
ehunter-usgs <ehunter@usgs.gov>
2014-02-14 09:51:06 -0700

    Build an observatory list, that when clicked shows a list of observations for that observatory. The list of observations should link to the readings/ observation details.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -3,14 +3,14 @@ if (!isset($TEMPLATE)) {
 	include_once '../conf/config.inc.php';
 	include_once 'functions.inc.php';
 
-	$TITLE = 'Observatories';
+	$TITLE = 'Observatory List';
 
 	$HEAD = '<link rel="stylesheet" href="' . $MOUNT_PATH . '/css/index.css"/>';
 	$FOOT =
 		'<script>
 			var MOUNT_PATH = \'' . $MOUNT_PATH .'\';
 		</script>' .
-		'<script src="requirejs/require.js" data-main="' . $MOUNT_PATH . '/js/index.js"></script>';
+		'<script src="' . $MOUNT_PATH . '/js/index.js"></script>';
 
 	include 'template.inc.php';
 }
--- src/htdocs/js/geomag/ObservatoryView.js
+++ src/htdocs/js/geomag/ObservatoryView.js
@@ -1,17 +1,19 @@
 /* global define */
 define([
 	'mvc/View',
-	'util/Util'
+	'util/Util',
+	'geomag/ObservatoryFactory'
 ], function (
 	View,
-	Util
+	Util,
+	ObservatoryFactory
 ) {
 	'use strict';
 
 
 	var DEFAULTS = {
+		factory: new ObservatoryFactory()
 	};
-
 
 	var ObservatoryView = function (options) {
 		this._options = Util.extend({}, DEFAULTS, options);
@@ -19,14 +21,96 @@ define([
 	};
 	ObservatoryView.prototype = Object.create(View.prototype);
 
-
 	ObservatoryView.prototype.render = function () {
 		// TODO :: Render current model
 	};
 
 	ObservatoryView.prototype._initialize = function () {
-		this._el.innerHTML = '<p>I am an ObservatoryView!</p>';
+		var _this = this,
+		    el = this._el,
+		    factory = this._options.factory;
+
+		el.innerHTML = [
+				'<section class="observatories"></section>',
+				'<section class="observations-view"></section>',
+		].join('');
+
+
+		// load observatories
+		factory.getObservatories({
+			success: function (observatories) {
+				_this._buildObservatoryList(observatories);
+			}
+		});
+
 	};
+
+
+	ObservatoryView.prototype._buildObservatoryList = function (data) {
+
+		var observatories = this._el.querySelector('.observatories'),
+		    observatoryList = document.createElement('ul'),
+		    observatory, markup = [];
+
+		observatories.innerHTML = '<h2>Observatories</h2>';
+
+		for (var i = 0; i < data.length; i++) {
+			observatory = data[i];
+			markup.push('<li id=' + observatory.get('id') + '>' + observatory.get('name') + '</li>');
+		}
+
+		observatoryList.innerHTML = markup.join('');
+
+		observatories.appendChild(observatoryList);
+
+		var list = observatoryList.querySelectorAll('li');
+
+		this._bindObservatoryListItems(list);
+
+	};
+
+	ObservatoryView.prototype._bindObservatoryListItems = function (observatories) {
+		var _this = this,
+		    factory = this._options.factory;
+
+		for (var i = 0; i < observatories.length; i++) {
+			// TODO: add onClick handler for list
+			Util.addEvent(observatories[i], 'click', function () {
+				factory.getObservatory({
+					id: this.id,
+					success: function (data) {
+						_this._buildObservationList(data);
+					}
+				});
+			});
+		}
+
+	};
+
+
+	ObservatoryView.prototype._buildObservationList = function (observatory) {
+		var observationsView = this._el.querySelector('.observations-view'),
+		    observations = observatory.get('observations').data(),
+		    observation, list, markup = [];
+
+		observationsView.innerHTML = [
+				'<h2>', observatory.get('name'),'</h2>',
+				'<small>',
+					observatory.get('latitude') ,', ', observatory.get('longitude') ,
+				'</small>',
+				'<ul class="observations"></ul>'
+			].join('');
+
+		list = observationsView.querySelector('.observations');
+
+		for (var i = 0; i < observations.length; i++) {
+			observation = observations[i];
+			markup.push('<li><a href="/observation/' + observation.get('id') + '">Observation ' + observation.get('id') + '</a></li>');
+		}
+
+		list.innerHTML = markup.join('');
+	};
+
 
 
 	return ObservatoryView;
