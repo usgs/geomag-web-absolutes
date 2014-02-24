ec03f856ab0a711011674aeb7dc9a27381061bf5
Eddie McWhirter <emcwhirter@usgs.gov>
2014-02-24 15:41:30 -0700

    Moved degree-inversion-test from MeasurementViewTest to FormatterTest

--- test/spec/MeasurementViewTest.js
+++ test/spec/MeasurementViewTest.js
@@ -24,6 +24,7 @@ define([
 	};
 
 	describe('MeasurementView Unit Tests', function () {
+
 		describe('Constructor', function () {
 			var m = new MeasurementView(viewOptions);
 
@@ -32,24 +33,6 @@ define([
 			});
 		});
 
-		// These are currently passing but take a long time (~10 sec) to complete.
-		// We should re-check this test any time the _dmsToDecimal or _decimalToDms
-		// methods are updated
-		describe.skip('degree_inversion_check', function () {
-			var m = new MeasurementView(viewOptions);
-
-			it('gives back original input', function () {
-				var deg, min, sec, result;
-
-				for (deg = 0; deg < 360; deg++) {
-					for (min = 0; min < 60; min++) {
-						for (sec = 0; sec < 60; sec++) {
-							result = m._dmsToDecimal(deg, min, sec);
-							expect(m._decimalToDms(result)).to.deep.equal([deg, min, sec]);
-						}
-					}
-				}
-			});
-		});
 	});
+
 });
