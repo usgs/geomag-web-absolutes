c4e4d8c69388d7b6634deb450a351809ca1f3d76
Jonathan Brown <jdbrown@usgs.gov>
2014-02-25 15:56:36 -0700

    Removed blank space from element

--- src/htdocs/js/geomag/HorizontalIntensitySummaryView.js
+++ src/htdocs/js/geomag/HorizontalIntensitySummaryView.js
@@ -54,7 +54,7 @@ define([
 		    i = null,
 			  len = null;
 		el.innerHTML = [
-			'<th class="name" scope="row">&nbsp;</th>',
+			'<th class="name" scope="row"></th>',
 			'<td class="valid"><input type="checkbox" /></td>',
 			'<td class="start-time"></td>',
 			'<td class="end-time"></td>',
