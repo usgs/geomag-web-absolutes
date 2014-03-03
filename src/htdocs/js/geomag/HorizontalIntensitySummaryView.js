0cad2d34be0864021dbf7ff5384012bd5ae8ac3e
Jonathan Brown <jdbrown@usgs.gov>
2014-03-03 08:01:36 -0700

    Cleaned up code

--- src/htdocs/js/geomag/HorizontalIntensitySummaryView.js
+++ src/htdocs/js/geomag/HorizontalIntensitySummaryView.js
@@ -57,6 +57,7 @@ define([
 		var el = this._el,
 		    i = null,
 		    len = null;
+
 		el.innerHTML = [
 			'<th class="name" scope="row"></th>',
 			'<td class="valid"><input type="checkbox" /></td>',
