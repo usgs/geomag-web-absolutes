637b67789cf012697cb326f89c85202d5f6e36ff
Eric Martinez <emartinez@usgs.gov>
2014-03-04 09:11:55 -0700

    Removed references to old configuration stuff.

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -11,11 +11,6 @@ $DATA_DIR = $CONFIG['DATA_DIR'];
 $MOUNT_PATH = $CONFIG['MOUNT_PATH'];
 
 
-$dburl = 'sqlite:' . $DATA_DIR . DIRECTORY_SEPARATOR . $CONFIG['SQLITE_DB'];
-$dbschema = $DATA_DIR . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR .
-		'sql' . DIRECTORY_SEPARATOR . 'sqlite' . DIRECTORY_SEPARATOR .
-		'create_tables.sql';
-
 include_once $APP_DIR . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR .
 		'lib.inc.php';
 
