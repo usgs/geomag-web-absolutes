f040c57b988deb17399a828f8ba0a434271c1561
Eddie McWhirter <emcwhirter@usgs.gov>
2014-09-24 13:14:30 -0600

    Remove all but observatory information from sql files, combine into one file, delete all other files

---
+++ src/lib/sql/reference_data/load_observatories.sql
@@ -0,0 +1,59 @@
+/* BDT - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation, default_pier_id)
+VALUES ('Boulder Test (BDT)','BDT','Boulder, CO','40.1375','-105.2372','48.40','320.59','1682','HDZF',NULL);
+
+/* BOU - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation, default_pier_id)
+VALUES ('Boulder (BOU)','BOU','Boulder, CO','40.1375','-105.2372','48.40','320.59','1682','HDZF',NULL);
+
+/* BRW - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Barrow (BRW)','BRW','Point Barrow, AK','71.3225','-156.6231','69.61','246.26','12','HDZF');
+
+/* BSL - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Stennis (BSL)','BSL','NASA Stennis Space Center, MS','30.3507','-89.6359','40.05','339.79','8','HDZF');
+
+/* CMO - observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation, default_pier_id)
+VALUES ('College (CMO)','CMO','Fairbanks, AK','64.8742','-147.8597','65.38','261.63','197','HDZF',NULL);
+
+/* DED - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Deadhorse (DED)','DED','Deadhorse, AK','70.3552','-148.7928',NULL,NULL,'10','HDZF');
+
+/* FRD - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Fredericksburg (FRD)','FRD','Corbin, VA','38.2047','-77.3729','48.40','353.38','69','HDZF');
+
+/* FRN - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Fresno (FRN)','FRN','ONeals, CA','37.0913','-119.7193','43.52','305.25','331','HDZF');
+
+/* GUA - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Guam (GUA)','GUA','Dededo, Guam','13.5895','144.8694','5.30','215.64','140','HDZF');
+
+/* HON - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Honolulu (HON)','HON','Ewa Beach, HI','21.3166','-157.9996','21.64','269.74','4','HDZF');
+
+/* NEW - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Newport (NEW)','NEW','Colville National Forest, WA','48.2649','-117.1231','54.85','304.68','770','HDZF');
+
+/* SHU - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Shumagin (SHU)','SHU','Sand Point, Popof, AK','55.3472','-160.4644','256.78','160.46','80','HDZF');
+
+/* SIT - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Sitka (SIT)','SIT','Sitka, AK','57.0576','-135.3273','60.34','280.35','24','HDZF');
+
+/* SJG - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('San Juan (SJG)','SJG','Cayey, PR','18.1110','-66.1498','28.31','6.08','424','HDZF');
+
+/* TUC - add observatory entry */
+INSERT INTO observatory (name, code, location, latitude, longitude, geomagnetic_latitude, geomagnetic_longitude, elevation, orientation)
+VALUES ('Tucson (TUC)','TUC','Tucson, AZ','32.1745','-110.7337','39.88','316.11','946','HDZF');
