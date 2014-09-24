97b7b69749a482f20ecde4d0579dd30f225afa38
Eric Martinez <emartinez@usgs.gov>
2014-09-24 15:49:57 -0600

    Fixed typos. Fixed path to login file. Using global for CURRENT_USER.

--- src/htdocs/publish.php
+++ src/htdocs/publish.php
@@ -1,11 +1,11 @@
 <?php
 
 include_once '../conf/config.inc.php';
-include_once '../conf/pusblish.inc.php';
+include_once '../conf/publish.inc.php';
 include_once 'functions.inc.php';
 
-include_once 'login.inc.php';
 
+include_once '../lib/login.inc.php';
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 	header('HTTP/1.1 400 Bad Request');
 	echo 'Should only post to publish.';
@@ -15,14 +15,14 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 $observationId = param('id', null);
 try {
 	$observation = $OBSERVATION_FACTORY->getObservation($observationId);
-	$observer = $USER_FACTORY->getById($observation->observer_user_id);
-	$reviewer = $USER_FACTORY->getCurrentUser();
+	$observer = $USER_FACTORY->getUser($observation->observer_user_id);
+	$reviewer = $CURRENT_USER;
 
 	if ($reviewer['admin'] === 'Y') {
 		if ($observation->reviewed !== 'Y') {
 			$MAGPROC_FACTORY->publish($observation, $observer['username'],
 					$reviewer['username']);
-			$observation->reviewer_user_id = $reviewer['id'];
+			$observation->reviewer_user_id = $reviewer['ID'];
 			$observation->reviewed = 'Y';
 			$OBSERVATION_FACTORY->updateObservation($observation);
 			header('Content-Type: application/json');
