a0d324792986f9997ad993f4f799a50cf20a197e
Eric Martinez <emartinez@usgs.gov>
2014-09-25 12:28:38 -0600

    Set error mode to throw exceptions for magprocdb.

--- src/conf/publish.inc.php
+++ src/conf/publish.inc.php
@@ -4,5 +4,6 @@ include_once '../conf/config.inc.php';
 
 $MAGPROCDB = new PDO($CONFIG['MAGPROC_DSN'], $CONFIG['MAGPROC_USER'],
 			$CONFIG['MAGPROC_PASS']);
+$MAGPROCDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $MAGPROC_FACTORY = new MagProcPublisher($MAGPROCDB);
