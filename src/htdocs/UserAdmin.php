005242bb4982f02305a362f760ed64fbad6260c3
Jonathan Brown <jdbrown@usgs.gov>
2014-09-25 13:28:22 -0600

    set up user admin

---
+++ src/htdocs/UserAdmin.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($TEMPLATE)) {
+	include_once '../conf/config.inc.php';
+	include_once 'functions.inc.php';
+	include '../lib/login.inc.php';
+	include 'template.inc.php';
+
+	if ($CURRENT_USERS['admin'] !== 'Y') {
+		header('HTTP/1.1 403 Forbiden');
+		echo 'Only admin users may view or change user data.';
+	}
+	$TITLE = 'User Admin';
+
+	$NAVIGATION = true;
+	$HEAD = '<link rel="stylesheet" href="'.$MOUNT_PATH.'/css/UserAdmin.css"/>';
+	$FOOT =
+		'<script>
+			var MOUNT_PATH = \'' . $MOUNT_PATH .'\';
+		</script>' .
+		'<script src="' . $MOUNT_PATH . '/js/UserAdmin.js"></script>' .
+		'<script src="http://localhost:35729/livereload.js?snipver=1"></script>';
+}
+?>
+
+<div class="user-admin-view-wrapper"></div>
