b1b2d266b40fc642a8aac5d39874fcc90e2dbaa9
Eddie McWhirter <emcwhirter@usgs.gov>
2014-10-02 16:06:06 -0600

    Allow non-admin users to see observers and reviewers usernames

--- src/htdocs/user_data.php
+++ src/htdocs/user_data.php
@@ -15,11 +15,13 @@ if ($id !== null) {
 $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] :
 		 'GET';
 
-if ($CURRENT_USER['admin'] !== 'Y') {
-	header('HTTP/1.1 403 Forbiden');
-	echo 'Only admin users may view or change user data.';
-	exit();
-}
+// if ($CURRENT_USER['admin'] !== 'Y') {
+// function notAdmin () {
+// 	header('HTTP/1.1 403 Forbiden');
+// 	echo 'Only admin users may view or change user data.';
+// 	exit();
+// }
+admin = ($CURRENT_USER['admin'] === 'Y');
 
 // process request
 try {
@@ -50,7 +52,7 @@ try {
 			header('Content-Type: application/json');
 			echo $json;
 		}
-	} else if ($method === 'DELETE') {
+	} else if ($method === 'DELETE' && admin) {
 		// php doesn't populate $_POST when method is DELETE.
 		$params = array();
 		parse_str(file_get_contents('php://input'), $params);
@@ -87,7 +89,7 @@ try {
 		$json = json_decode($json, true /* associative */);
 		$user = $json; //User::fromArray($json);
 
-		if ($method === 'POST') {
+		if ($method === 'POST' && admin) {
 			// create
 			if ($user['id'] !== null) {
 				header('HTTP/1.1 400 Bad Request');
@@ -95,7 +97,7 @@ try {
 				exit();
 			}
 			$user = $USER_FACTORY->createUser($user);
-		} else if ($method === 'PUT') {
+		} else if ($method === 'PUT' && admin) {
 			// update
 			if ($user['id'] === null) {
 				header('HTTP/1.1 400 Bad Request');
@@ -103,6 +105,10 @@ try {
 				exit();
 			}
 			$user = $USER_FACTORY->updateUser($user);
+		} else if (!admin) {
+				header('HTTP/1.1 403 Forbiden');
+				echo 'Only admin users may view or change user data.';
+				exit();
 		} else {
 				header('HTTP/1.1 405 Method not allowed');
 				echo 'unsupported HTTP method';
