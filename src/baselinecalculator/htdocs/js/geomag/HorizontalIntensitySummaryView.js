5ec1bc7f478cd149ee42165b95deadf5a19de122
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-12 15:16:50 -0700

    Browserify HorizontalIntensitySummaryView and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/HorizontalIntensitySummaryView.js
+++ src/baselinecalculator/htdocs/js/geomag/HorizontalIntensitySummaryView.js
@@ -1,69 +1,54 @@
-/* global define */
-define([
-  'mvc/View',
-  'util/Util',
+'use strict';
 
-  'geomag/Formatter',
-  'geomag/Measurement',
-  'geomag/ObservatoryFactory'
-], function (
-  View,
-  Util,
-
-  Format,
-  Measurement,
-  ObservatoryFactory
-) {
-  'use strict';
+var Format = require('geomag/Formatter'),
+    Measurement = require('geomag/Measurement'),
+    ObservatoryFactory = require('geomag/ObservatoryFactory'),
+    Util = require('util/Util'),
+    View = require('mvc/View');
 
 
-  var DEFAULTS = {
-    factory: new ObservatoryFactory()
-  };
+var _DEFAULTS = {
+  factory: new ObservatoryFactory()
+};
 
 
-  var HorizontalIntensitySummaryView = function (options) {
-    this._options = Util.extend({}, DEFAULTS, options);
-    View.call(this, this._options);
-  };
-  HorizontalIntensitySummaryView.prototype = Object.create(View.prototype);
+/**
+ * Construct a new HorizontalIntensitySummaryView.
+ *
+ * @param option {Object}
+ *        view options.
+ * @param options.calculator {geomag.ObservationBaselineCalculator}
+ *        the calculator to use.
+ * @param options.factory {geomag.ObservatoryFactory}
+ *        the factory to use.
+ * @parem options.reading {geomag.Reading}
+ *        the reading to display.
+ */
+var HorizontalIntensitySummaryView = function (options) {
+  var _this,
+      _initialize,
 
+      _options,
 
-  HorizontalIntensitySummaryView.prototype.render = function () {
-    var reading = this._reading,
-        measurements = reading.get('measurements').data(),
-        factory = this._options.factory,
-        startTime = null,
-        endTime = null,
-        times;
+      _onChange;
 
-    this._name.innerHTML = reading.get('set_number');
-
-    this._valid.checked = (reading.get('horizontal_intensity_valid') === 'Y');
-
-    times = factory.getMeasurementValues(measurements, 'time');
-    if (times.length > 0) {
-      startTime = Math.min.apply(null, times);
-      endTime = Math.max.apply(null, times);
-    }
-    this._startTime.innerHTML = Format.time(startTime);
-    this._endTime.innerHTML = Format.time(endTime);
-
-    this._absValue.innerHTML =
-        Format.nanoteslas(this._calculator.horizontalComponent(reading));
-    this._ord.innerHTML =
-        Format.nanoteslas(this._calculator.meanH(reading));
-    this._baselineValues.innerHTML =
-        Format.nanoteslas(this._calculator.hBaseline(reading));
-    this._observer.innerHTML = this._reading.get('observer') || '';
-  };
-
-  HorizontalIntensitySummaryView.prototype._initialize = function () {
-    var el = this._el,
-        factory = this._options.factory,
-        reading = this._options.reading,
+  _this = View(options);
+  /**
+   * Initialize view, and call render.
+   * @param options {Object} same as constructor.
+   */
+  _initialize = function () {
+    var calculator,
+        el = _this.el,
+        factory,
+        reading,
         i = null,
         len = null;
+
+    _options = Util.extend({}, _DEFAULTS, options);
+    calculator = _options.calculator;
+    factory = _options.factory;
+    reading = _options.reading;
 
     el.innerHTML = [
       '<th class="name" scope="row"></th>',
@@ -76,37 +61,74 @@ define([
       '<td class="observer"></td>'
     ].join('');
 
-    this._name = el.querySelector('.name');
-    this._valid = el.querySelector('.valid > input');
-    this._startTime = el.querySelector('.start-time');
-    this._endTime = el.querySelector('.end-time');
-    this._absValue = el.querySelector('.abs-value');
-    this._ord = el.querySelector('.ord');
-    this._baselineValues = el.querySelector('.baseline-values');
-    this._observer = el.querySelector('.observer');
+    // save references to elements that will be updated during render
+    _this._name = el.querySelector('.name');
+    _this._valid = el.querySelector('.valid > input');
+    _this._startTime = el.querySelector('.start-time');
+    _this._endTime = el.querySelector('.end-time');
+    _this._absValue = el.querySelector('.abs-value');
+    _this._ord = el.querySelector('.ord');
+    _this._baselineValues = el.querySelector('.baseline-values');
+    _this._observer = el.querySelector('.observer');
 
-    this._reading = this._options.reading;
-    this._calculator = this._options.calculator;
+    _this._reading = reading;
+    _this._calculator = calculator;
 
-    this._measurements = factory.getHorizontalIntensityMeasurements(reading);
+    _this._measurements = factory.getHorizontalIntensityMeasurements(reading);
 
-    this._onChange = this._onChange.bind(this);
-    this._valid.addEventListener('change', this._onChange);
+    _this._valid.addEventListener('change', _onChange);
 
-    this._reading.on('change:horizontal_intensity_valid', this.render, this);
+    _this._reading.on('change:horizontal_intensity_valid', _this.render, _this);
 
-    this._calculator.on('change', this.render, this);
+    // watches for changes in pier/mark
+    _this._calculator.on('change', _this.render, _this);
 
-    for (i = 0, len = this._measurements.length; i < len; i ++){
-      this._measurements[i].on('change', this.render, this);
+    for (i = 0, len = _this._measurements.length; i < len; i ++){
+      _this._measurements[i].on('change', _this.render, _this);
     }
-    this.render();
+
+    _this.render();
   };
 
-  HorizontalIntensitySummaryView.prototype._onChange = function () {
-    this._reading.set({
-      horizontal_intensity_valid: (this._valid.checked ? 'Y' : 'N')
+  _onChange = function () {
+    _this._reading.set({
+      horizontal_intensity_valid: (_this._valid.checked ? 'Y' : 'N')
     });
   };
-  return HorizontalIntensitySummaryView;
-});
+
+  _this.render = function () {
+    var calculator = _this._calculator,
+        reading = _this._reading,
+        measurements = reading.get('measurements').data(),
+        factory = _options.factory,
+        startTime = null,
+        endTime = null,
+        times;
+
+    _this._name.innerHTML = reading.get('set_number');
+
+    _this._valid.checked = (reading.get('horizontal_intensity_valid') === 'Y');
+
+    times = factory.getMeasurementValues(measurements, 'time');
+    if (times.length > 0) {
+      startTime = Math.min.apply(null, times);
+      endTime = Math.max.apply(null, times);
+    }
+    _this._startTime.innerHTML = Format.time(startTime);
+    _this._endTime.innerHTML = Format.time(endTime);
+
+    _this._absValue.innerHTML =
+        Format.nanoteslas(calculator.horizontalComponent(reading));
+    _this._ord.innerHTML =
+        Format.nanoteslas(calculator.meanH(reading));
+    _this._baselineValues.innerHTML =
+        Format.nanoteslas(calculator.hBaseline(reading));
+    _this._observer.innerHTML = _this._reading.get('observer') || '';
+  };
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = HorizontalIntensitySummaryView;
