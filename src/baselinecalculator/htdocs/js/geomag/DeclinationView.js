e1d7d84e3fa6db33c98c838459f289834709e607
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-12 14:42:05 -0700

    Browserify DeclinationView and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/DeclinationView.js
+++ src/baselinecalculator/htdocs/js/geomag/DeclinationView.js
@@ -1,24 +1,15 @@
-/*global define*/
-define([
-  'mvc/View',
-  'util/Util',
+'use strict';
 
-  'geomag/Formatter',
-  'geomag/Measurement'
-], function (
-  View,
-  Util,
-
-  Format,
-  Measurement
-) {
-  'use strict';
+var Format = require('geomag/Formatter'),
+    Measurement = require('geomag/Measurement'),
+    Util = require('util/Util'),
+    View = require('mvc/View');
 
 
-  var DEFAULT_OPTIONS = {
-    'baselineCalculator': null,
-    'reading': null
-  };
+var _DEFAULT_OPTIONS = {
+  'baselineCalculator': null,
+  'reading': null
+};
 
 
   /**
@@ -31,24 +22,28 @@ define([
    * @param option.reading {geomag.Reading}
    *        the reading to display.
    */
-  var DeclinationView = function (options) {
-    this._options = Util.extend({}, DEFAULT_OPTIONS, options);
-    View.call(this, this._options);
-  };
-  DeclinationView.prototype = Object.create(View.prototype);
+var DeclinationView = function (options) {
+  var _this,
+      _initialize,
 
+      _options;
 
+  _this = View(options);
   /**
    * Initialize view, and call render.
    * @param options {Object} same as constructor.
    */
-  DeclinationView.prototype._initialize = function () {
-    this._reading = this._options.reading;
-    this._calculator = this._options.baselineCalculator;
-    this._measurements = this._reading.getMeasurements();
+  _initialize = function (options) {
+    var el = _this.el;
 
-    this._el.classList.add('declination-view');
-    this._el.innerHTML = [
+    _options = Util.extend({}, _DEFAULT_OPTIONS, options);
+
+    _this._reading = _options.reading;
+    _this._calculator = _options.baselineCalculator;
+    _this._measurements = _this._reading.getMeasurements();
+
+    el.classList.add('declination-view');
+    el.innerHTML = [
       '<dl>',
         '<dt class="mag-s-meridian">Magnetic South Meridian</dt>',
         '<dd class="mag-s-meridian-value">&ndash;</dd>',
@@ -69,73 +64,73 @@ define([
     ].join('');
 
     // save references to elements that will be updated during render
-    this._magneticSouthMeridian =
-        this._el.querySelector('.mag-s-meridian-value');
-    this._meanMark = this._el.querySelector('.mean-mark-value');
-    this._magneticAzimuthOfMark =
-        this._el.querySelector('.mag-az-of-mark-value');
-    this._trueAzimuthOfMark = this._el.querySelector('.true-az-of-mark-value');
-    this._magneticDeclination = this._el.querySelector(
-        '.mag-declination-value');
+    _this._magneticSouthMeridian = el.querySelector('.mag-s-meridian-value');
+    _this._meanMark = el.querySelector('.mean-mark-value');
+    _this._magneticAzimuthOfMark = el.querySelector('.mag-az-of-mark-value');
+    _this._trueAzimuthOfMark = el.querySelector('.true-az-of-mark-value');
+    _this._magneticDeclination = el.querySelector('.mag-declination-value');
 
-    this._westUpMinusEastDown = this._el.querySelector(
-        '.west-up-minus-east-down-value');
-    this._eastUpMinusWestDown = this._el.querySelector(
-        '.east-up-minus-west-down-value');
+    _this._westUpMinusEastDown =
+        el.querySelector('.west-up-minus-east-down-value');
+    _this._eastUpMinusWestDown =
+        el.querySelector('.east-up-minus-west-down-value');
 
     // when reading changes render view
-    this._options.reading.on('change', this.render, this);
+    _options.reading.on('change', _this.render, _this);
 
     // also render when any related inputs change
-    this._measurements[Measurement.FIRST_MARK_UP][0].on(
-        'change', this.render, this);
-    this._measurements[Measurement.FIRST_MARK_DOWN][0].on(
-        'change', this.render, this);
+    _this._measurements[Measurement.FIRST_MARK_UP][0].on(
+        'change', _this.render, _this);
+    _this._measurements[Measurement.FIRST_MARK_DOWN][0].on(
+        'change', _this.render, _this);
 
-    this._measurements[Measurement.WEST_DOWN][0].on(
-        'change', this.render, this);
-    this._measurements[Measurement.EAST_DOWN][0].on(
-        'change', this.render, this);
-    this._measurements[Measurement.WEST_UP][0].on(
-        'change', this.render, this);
-    this._measurements[Measurement.EAST_UP][0].on(
-        'change', this.render, this);
+    _this._measurements[Measurement.WEST_DOWN][0].on(
+        'change', _this.render, _this);
+    _this._measurements[Measurement.EAST_DOWN][0].on(
+        'change', _this.render, _this);
+    _this._measurements[Measurement.WEST_UP][0].on(
+        'change', _this.render, _this);
+    _this._measurements[Measurement.EAST_UP][0].on(
+        'change', _this.render, _this);
 
-    this._measurements[Measurement.SECOND_MARK_UP][0].on(
-        'change', this.render, this);
-    this._measurements[Measurement.SECOND_MARK_DOWN][0].on(
-        'change', this.render, this);
+    _this._measurements[Measurement.SECOND_MARK_UP][0].on(
+        'change', _this.render, _this);
+    _this._measurements[Measurement.SECOND_MARK_DOWN][0].on(
+        'change', _this.render, _this);
 
     // watches for changes in pier/mark
-    this._calculator.on('change', this.render, this);
+    _this._calculator.on('change', _this.render, _this);
 
     // render current reading
-    this.render();
+    _this.render();
   };
 
   /**
    * Update view based on current reading values.
    */
-  DeclinationView.prototype.render = function () {
+  _this.render = function () {
+    var calculator = _this._calculator,
+        reading = _this._reading;
 
-    var calculator = this._calculator,
-        reading = this._reading;
-
-    this._magneticSouthMeridian.innerHTML =
+    _this._magneticSouthMeridian.innerHTML =
         Format.degreesAndDegreesMinutes(calculator.magneticSouthMeridian(reading));
-    this._meanMark.innerHTML = Format.degrees(calculator.meanMark(reading));
-    this._magneticAzimuthOfMark.innerHTML =
+    _this._meanMark.innerHTML = Format.degrees(calculator.meanMark(reading));
+    _this._magneticAzimuthOfMark.innerHTML =
         Format.degrees(calculator.magneticAzimuthMark(reading));
-    this._trueAzimuthOfMark.innerHTML =
+    _this._trueAzimuthOfMark.innerHTML =
         Format.rawDegrees(calculator.trueAzimuthOfMark());
-    this._magneticDeclination.innerHTML =
+    _this._magneticDeclination.innerHTML =
         Format.degreesAndDegreesMinutes(calculator.magneticDeclination(reading));
 
-    this._westUpMinusEastDown.innerHTML =
+    _this._westUpMinusEastDown.innerHTML =
         Format.minutes(calculator.westUpMinusEastDown(reading)*60);
-    this._eastUpMinusWestDown.innerHTML =
+    _this._eastUpMinusWestDown.innerHTML =
         Format.minutes(calculator.eastUpMinusWestDown(reading)*60);
   };
 
-  return DeclinationView;
-});
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = DeclinationView;
