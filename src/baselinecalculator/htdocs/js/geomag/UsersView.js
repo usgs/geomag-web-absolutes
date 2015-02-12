f7f20c2c441d8c7d704ddaefa5cfa2fc2d1e06cf
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-12 15:51:03 -0700

    Browserify UsersView and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/UsersView.js
+++ src/baselinecalculator/htdocs/js/geomag/UsersView.js
@@ -1,84 +1,81 @@
-/* global define */
-define([
-  'geomag/Formatter',
+'use strict';
 
-  'mvc/CollectionTable',
-  'util/Util'
-], function (
-  Formatter,
+var CollectionTable = require('mvc/CollectionTable'),
+    Format = require('geomag/Formatter'),
+    Util = require('util/Util');
 
-  CollectionTable,
-  Util
-) {
-  'use strict';
 
-  var DEFAULTS = {
-    className: 'collection-table users-view tabular',
-    clickToSelect: false,
-    columns: [
-      {
-        className: 'username',
-        title: 'Username',
-        format: function (user) {
-          return user.get('username');
-        }
-      },
-      {
-        className: 'email',
-        title: 'Email',
-        format: function (user) {
-          return user.get('email');
-        }
-      },
-      {
-        className: 'name',
-        title: 'Name',
-        format: function (user) {
-          return user.get('name');
-        }
-      },
-      {
-        className: 'admin',
-        title: 'Admin',
-        format: function (user) {
-          return user.get('admin');
-        }
-      },
-      {
-        className: 'enabled',
-        title: 'Enabled',
-        format: function (user) {
-          return user.get('enabled');
-        }
-      },
-      {
-        className: 'last_login',
-        title: 'Last Login',
-        format: function (user) {
-          if (user.get('last_login') === null) {
-            return '&ndash;';
-          }
-          return Formatter.date(parseInt(user.get('last_login'), 10)*1000);
-        }
-      },
-      {
-        className: 'edit',
-        title: '',
-        format: function (user) {
-          return '<button class="edituser" data-id="' + user.get('id') +
-              '">Edit</button>';
-        }
+var _DEFAULTS = {
+  className: 'collection-table users-view tabular',
+  clickToSelect: false,
+  columns: [
+    {
+      className: 'username',
+      title: 'Username',
+      format: function (user) {
+        return user.get('username');
       }
-    ],
-    emptyMarkup: '&ndash;'
-  };
+    },
+    {
+      className: 'email',
+      title: 'Email',
+      format: function (user) {
+        return user.get('email');
+      }
+    },
+    {
+      className: 'name',
+      title: 'Name',
+      format: function (user) {
+        return user.get('name');
+      }
+    },
+    {
+      className: 'admin',
+      title: 'Admin',
+      format: function (user) {
+        return user.get('admin');
+      }
+    },
+    {
+      className: 'enabled',
+      title: 'Enabled',
+      format: function (user) {
+        return user.get('enabled');
+      }
+    },
+    {
+      className: 'last_login',
+      title: 'Last Login',
+      format: function (user) {
+        if (user.get('last_login') === null) {
+          return '&ndash;';
+        }
+        return Formatter.date(parseInt(user.get('last_login'), 10)*1000);
+      }
+    },
+    {
+      className: 'edit',
+      title: '',
+      format: function (user) {
+        return '<button class="edituser" data-id="' + user.get('id') +
+            '">Edit</button>';
+      }
+    }
+  ],
+  emptyMarkup: '&ndash;'
+};
 
-  var UsersView = function (options) {
-    options = Util.extend({}, DEFAULTS, options || {});
-    CollectionTable.call(this, options);
-  };
-  UsersView.prototype = Object.create(CollectionTable.prototype);
 
+var UsersView = function (options) {
+  var _this,
 
-  return UsersView;
-});
+      _options;
+
+  _this = CollectionTable(options);
+  _options = Util.extend({}, _DEFAULTS, options || {});
+
+  return _this;
+};
+
+module.export = UsersView;
