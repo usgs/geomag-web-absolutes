018434209081c1ba89e642149de8b239230b7da0
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-12 12:44:04 -0700

    Copy app, conf and lib from Gruntfile to copy.

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -10,6 +10,35 @@ var copy = {
       'test.html'
     ],
     dest: config.build + '/' + config.test
+  },
+  app: {
+    expand: true,
+    options: {mode: true},
+    cwd: config.src + '/htdocs',
+    dest: config.dist + '/htdocs',
+    src: [
+      'img/**/*.{png,gif,jpg,jpeg}',
+      '**/*.php'
+    ]
+  },
+  conf: {
+    expand: true,
+    options: {mode: true},
+    cwd: config.src + '/conf',
+    dest: config.dist + '/conf',
+    src: [
+      'config.inc.php',
+      'config.ini'
+    ]
+  },
+  lib: {
+    expand: true,
+    options: {mode: true},
+    cwd: config.src + '/lib',
+    dest: config.dist + '/lib',
+    src: [
+      '**/*'
+    ]
   }
 };
 
