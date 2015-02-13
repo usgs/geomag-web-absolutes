8f37a9b6b0e4c7d8033cdb34b1816d6c7fb4408b
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-13 15:14:30 -0700

    Browserify ReadingView and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/ReadingView.js
+++ src/baselinecalculator/htdocs/js/geomag/ReadingView.js
@@ -1,69 +1,67 @@
-/* global define */
-define([
-  'mvc/View',
-  'util/Util',
+'use strict';
 
-  'geomag/DeclinationView',
-  'geomag/InclinationView',
-  'geomag/MagnetometerOrdinatesView',
-  'geomag/Measurement',
-  'geomag/MeasurementView',
-  'geomag/ObservationBaselineCalculator'
-], function (
-  View,
-  Util,
-
-  DeclinationView,
-  InclinationView,
-  MagnetometerOrdinatesView,
-  Measurement,
-  MeasurementView,
-  ObservationBaselineCalculator
-) {
-  'use strict';
+var DeclinationView = require('geomag/DeclinationView'),
+    InclinationView = require('geomag/InclinationView'),
+    MagnetometerOrdinatesView = require('geomag/MagnetometerOrdinatesView'),
+    Measurement = require('geomag/Measurement'),
+    MeasurementView = require('geomag/MeasurementView'),
+    ObservationBaselineCalculator = require('geomag/ObservationBaselineCalculator'),
+    Util = require('util/Util'),
+    View = require('mvc/View');
 
 
-  var DEFAULTS = {
-    baselineCalculator:new ObservationBaselineCalculator()
-  };
+var _DEFAULTS = {
+  baselineCalculator:new ObservationBaselineCalculator()
+};
 
 
-  var ReadingView = function (options) {
-    this._options = Util.extend({}, DEFAULTS, options);
-    View.call(this, this._options);
-  };
-  ReadingView.prototype = Object.create(View.prototype);
+  /**
+   * Construct a new DeclinationSummaryView.
+   *
+   * @param options {Object}
+   *        view options.
+   * @param options.baselineCalculator
+   * @param options.observation
+   * @param options.reading
+   */
+var ReadingView = function (options) {
+  var _this,
+      _initialize,
 
+      _options;
 
-  ReadingView.prototype.render = function () {
-    // TODO :: Render current model
-  };
+  _this = View(options);
+    /**
+   * Initialize view, and call render.
+   * @param options {Object} same as constructor.
+   */
+  _initialize = function (options) {
+    _options = Util.extend({}, _DEFAULTS, options);
 
-  ReadingView.prototype._initialize = function () {
-    this._observation = this._options.observation;
-    this._reading = this._options.reading;
-    this._calculator = this._options.baselineCalculator;
-    this._measurements = this._reading.getMeasurements();
+    _this._observation = _options.observation;
+    _this._reading = _options.reading;
+    _this._calculator = _options.baselineCalculator;
+    _this._measurements = _this._reading.getMeasurements();
 
-    this._firstMarkUpMeasurement =
-        this._measurements[Measurement.FIRST_MARK_UP][0];
-    this._firstMarkDownMeasurement =
-        this._measurements[Measurement.FIRST_MARK_DOWN][0];
-    this._westDownMeasurement = this._measurements[Measurement.WEST_DOWN][0];
-    this._eastDownMeasurement = this._measurements[Measurement.EAST_DOWN][0];
-    this._westUpMeasurement = this._measurements[Measurement.WEST_UP][0];
-    this._eastUpMeasurement = this._measurements[Measurement.EAST_UP][0];
-    this._secondMarkUpMeasurement =
-        this._measurements[Measurement.SECOND_MARK_UP][0];
-    this._secondMarkDownMeasurement =
-        this._measurements[Measurement.SECOND_MARK_DOWN][0];
+    _this._firstMarkUpMeasurement =
+        _this._measurements[Measurement.FIRST_MARK_UP][0];
+    _this._firstMarkDownMeasurement =
+        _this._measurements[Measurement.FIRST_MARK_DOWN][0];
+    _this._westDownMeasurement = _this._measurements[Measurement.WEST_DOWN][0];
+    _this._eastDownMeasurement = _this._measurements[Measurement.EAST_DOWN][0];
+    _this._westUpMeasurement = _this._measurements[Measurement.WEST_UP][0];
+    _this._eastUpMeasurement = _this._measurements[Measurement.EAST_UP][0];
+    _this._secondMarkUpMeasurement =
+        _this._measurements[Measurement.SECOND_MARK_UP][0];
+    _this._secondMarkDownMeasurement =
+        _this._measurements[Measurement.SECOND_MARK_DOWN][0];
 
-    this._southDownMeasurement = this._measurements[Measurement.SOUTH_DOWN][0];
-    this._northUpMeasurement = this._measurements[Measurement.NORTH_UP][0];
-    this._southUpMeasurement = this._measurements[Measurement.SOUTH_UP][0];
-    this._northDownMeasurement = this._measurements[Measurement.NORTH_DOWN][0];
+    _this._southDownMeasurement = _this._measurements[Measurement.SOUTH_DOWN][0];
+    _this._northUpMeasurement = _this._measurements[Measurement.NORTH_UP][0];
+    _this._southUpMeasurement = _this._measurements[Measurement.SOUTH_UP][0];
+    _this._northDownMeasurement = _this._measurements[Measurement.NORTH_DOWN][0];
 
-    this._el.innerHTML = [
+    _this.el.innerHTML = [
       '<section class="reading-view">',
         '<section class="row">',
           '<h2>Declination</h2>',
@@ -130,103 +128,110 @@ define([
       '</section>'
     ].join('');
 
-    this._firstMarkUpView = new MeasurementView({
-      el: this._el.querySelector('.first-mark-up'),
-      measurement: this._firstMarkUpMeasurement,
-      observation: this._observation
+    _this._firstMarkUpView = new MeasurementView({
+      el: _this.el.querySelector('.first-mark-up'),
+      measurement: _this._firstMarkUpMeasurement,
+      observation: _this._observation
     });
 
-    this._firstMarkDownView = new MeasurementView({
-      el: this._el.querySelector('.first-mark-down'),
-      measurement: this._firstMarkDownMeasurement,
-      observation: this._observation
+    _this._firstMarkDownView = new MeasurementView({
+      el: _this.el.querySelector('.first-mark-down'),
+      measurement: _this._firstMarkDownMeasurement,
+      observation: _this._observation
     });
 
-    this._westDownView = new MeasurementView({
-      el: this._el.querySelector('.west-down'),
-      measurement: this._westDownMeasurement,
-      observation: this._observation
+    _this._westDownView = new MeasurementView({
+      el: _this.el.querySelector('.west-down'),
+      measurement: _this._westDownMeasurement,
+      observation: _this._observation
     });
 
-    this._eastDownView = new MeasurementView({
-      el: this._el.querySelector('.east-down'),
-      measurement: this._eastDownMeasurement,
-      observation: this._observation
+    _this._eastDownView = new MeasurementView({
+      el: _this.el.querySelector('.east-down'),
+      measurement: _this._eastDownMeasurement,
+      observation: _this._observation
     });
 
-    this._westUpView = new MeasurementView({
-      el: this._el.querySelector('.west-up'),
-      measurement: this._westUpMeasurement,
-      observation: this._observation
+    _this._westUpView = new MeasurementView({
+      el: _this.el.querySelector('.west-up'),
+      measurement: _this._westUpMeasurement,
+      observation: _this._observation
     });
 
-    this._eastUpView = new MeasurementView({
-      el: this._el.querySelector('.east-up'),
-      measurement: this._eastUpMeasurement,
-      observation: this._observation
+    _this._eastUpView = new MeasurementView({
+      el: _this.el.querySelector('.east-up'),
+      measurement: _this._eastUpMeasurement,
+      observation: _this._observation
     });
 
-    this._secondMarkUpView = new MeasurementView({
-      el: this._el.querySelector('.second-mark-up'),
-      measurement: this._secondMarkUpMeasurement,
-      observation: this._observation
+    _this._secondMarkUpView = new MeasurementView({
+      el: _this.el.querySelector('.second-mark-up'),
+      measurement: _this._secondMarkUpMeasurement,
+      observation: _this._observation
     });
 
-    this._secondMarkDownView = new MeasurementView({
-      el: this._el.querySelector('.second-mark-down'),
-      measurement: this._secondMarkDownMeasurement,
-      observation: this._observation
+    _this._secondMarkDownView = new MeasurementView({
+      el: _this.el.querySelector('.second-mark-down'),
+      measurement: _this._secondMarkDownMeasurement,
+      observation: _this._observation
     });
 
-    this._declinationView = new DeclinationView({
-      el: this._el.querySelector('.declination-output'),
-      reading: this._reading,
-      observation: this._observation,
-      baselineCalculator: this._calculator
-    });
-
-
-    this._southDownView = new MeasurementView({
-      el: this._el.querySelector('.south-down'),
-      measurement: this._southDownMeasurement,
-      observation: this._observation
-    });
-
-    this._northUpView = new MeasurementView({
-      el: this._el.querySelector('.north-up'),
-      measurement: this._northUpMeasurement,
-      observation: this._observation
-    });
-
-    this._southUpView = new MeasurementView({
-      el: this._el.querySelector('.south-up'),
-      measurement: this._southUpMeasurement,
-      observation: this._observation
-    });
-
-    this._northDownView = new MeasurementView({
-      el: this._el.querySelector('.north-down'),
-      measurement: this._northDownMeasurement,
-      observation: this._observation
-    });
-
-    this._inclinationView = new InclinationView({
-      el: this._el.querySelector('.inclination-output'),
-      reading: this._reading,
-      observation: this._observation,
-      baselineCalculator: this._calculator
+    _this._declinationView = new DeclinationView({
+      el: _this.el.querySelector('.declination-output'),
+      reading: _this._reading,
+      observation: _this._observation,
+      baselineCalculator: _this._calculator
     });
 
 
-    this._magnetometerOrdinatesView = new MagnetometerOrdinatesView({
-      el: this._el.querySelector('.magnetometer-ordinates-output'),
-      reading: this._reading,
-      observation: this._observation,
-      baselineCalculator: this._calculator
+    _this._southDownView = new MeasurementView({
+      el: _this.el.querySelector('.south-down'),
+      measurement: _this._southDownMeasurement,
+      observation: _this._observation
+    });
+
+    _this._northUpView = new MeasurementView({
+      el: _this.el.querySelector('.north-up'),
+      measurement: _this._northUpMeasurement,
+      observation: _this._observation
+    });
+
+    _this._southUpView = new MeasurementView({
+      el: _this.el.querySelector('.south-up'),
+      measurement: _this._southUpMeasurement,
+      observation: _this._observation
+    });
+
+    _this._northDownView = new MeasurementView({
+      el: _this.el.querySelector('.north-down'),
+      measurement: _this._northDownMeasurement,
+      observation: _this._observation
+    });
+
+    _this._inclinationView = new InclinationView({
+      el: _this.el.querySelector('.inclination-output'),
+      reading: _this._reading,
+      observation: _this._observation,
+      baselineCalculator: _this._calculator
+    });
+
+
+    _this._magnetometerOrdinatesView = new MagnetometerOrdinatesView({
+      el: _this.el.querySelector('.magnetometer-ordinates-output'),
+      reading: _this._reading,
+      observation: _this._observation,
+      baselineCalculator: _this._calculator
     });
   };
 
 
-  return ReadingView;
-});
+  _this.render = function () {
+    // TODO :: Render current model
+  };
 
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = ReadingView;
