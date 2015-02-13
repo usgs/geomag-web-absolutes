d27e1df536671dbfc66250831200c66362213c82
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-13 10:51:35 -0700

    Browserify RealtimeDataFactory and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/RealtimeDataFactory.js
+++ src/baselinecalculator/htdocs/js/geomag/RealtimeDataFactory.js
@@ -1,71 +1,61 @@
-/*global define*/
+'use strict';
 
-define([
-  'mvc/Model',
-  'util/Util',
-  'util/Xhr',
-
-  'geomag/RealtimeData'
-], function (
-  Model,
-  Util,
-  Xhr,
-
-  RealtimeData
-) {
-  'use strict';
+var Model = require('mvc/Model'),
+    RealtimeData = require('geomag/RealtimeData'),
+    Util = require('util/Util'),
+    Xhr = require('util/Xhr');
 
 
-  var DEFAULT_URL = '/map/observatories_data.json.php';
+var _DEFAULT_URL = '/map/observatories_data.json.php';
 
-  /** Define default attributes. */
-  var DEFAULTS = {
-    'url': DEFAULT_URL,
-    'starttime': null,
-    'endtime': null,
-    'channels': ['H','E','Z','F'],
-    'freq': 'seconds'
-  };
+var _DEFAULTS = {
+  'url': _DEFAULT_URL,
+  'starttime': null,
+  'endtime': null,
+  'channels': ['H','E','Z','F'],
+  'freq': 'seconds'
+};
 
 
-  /**
-   * Constructor.
-   *
-   * @param options {Object} observatory attributes.
-   * @param options.url {string}
-   * @param options.channels {Array<string>}
-   * @param options.observatory {Array{string}}
-   * @param options.freq {string{seconds|minutes}}
-   * @param options.success {callback()}
-   */
-  var RealtimeDataFactory = function (options) {
-    // Call parent constructor
-    Model.call(this, Util.extend({}, DEFAULTS, options));
+/**
+ * Constructor.
+ *
+ * @param options {Object} observatory attributes.
+ * @param options.url {string}
+ * @param options.channels {Array<string>}
+ * @param options.observatory {Array{string}}
+ * @param options.freq {string{seconds|minutes}}
+ * @param options.success {callback()}
+ */
+var RealtimeDataFactory = function (options) {
+  var _this,
+      _initialize,
+
+      _options;
+
+    _this = Model(options);
+
+    _options = Util.extend({}, _DEFAULTS, options);
 
     // TODO: this is a hack to deal with
     // https://github.com/usgs/hazdev-webutils/issues/8
-    this._lastcall = null;
-  };
-
-  // RealtimeDataFactory extends Model
-  RealtimeDataFactory.prototype = Object.create(Model.prototype);
-
+    _this._lastcall = null;
 
   /**
    * @param options {Object} observatory attributes.
    *        options.???  Same as constructor.
    */
-  RealtimeDataFactory.prototype.getRealtimeData = function (options) {
-    options = Util.extend({}, this.get(), options);
+  _this.getRealtimeData = function (options) {
+    _options = Util.extend({}, this.get(), options);
 
     // TODO: this is a hack to deal with
     // https://github.com/usgs/hazdev-webutils/issues/8
-    if (this._lastcall !== null) {
-      while (this._lastcall === new Date().getTime()) {
+    if (_this._lastcall !== null) {
+      while (_this._lastcall === new Date().getTime()) {
         // wait until its not
       }
     }
-    this._lastcall = new Date().getTime();
+    _this._lastcall = new Date().getTime();
 
     Xhr.jsonp({
       url: options.url,
@@ -82,9 +72,8 @@ define([
     });
   };
 
+  return _this;
+};
 
-  // return constructor from closure
-  return RealtimeDataFactory;
-});
-
+module.exports = RealtimeDataFactory;
 
