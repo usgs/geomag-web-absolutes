86599eb7ff85eb091068107527d9566995f7c9c3
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-13 10:32:28 -0700

    Browserify Observatory and update syntax and formatting per styleguide.

--- src/baselinecalculator/htdocs/js/geomag/Observatory.js
+++ src/baselinecalculator/htdocs/js/geomag/Observatory.js
@@ -1,55 +1,48 @@
-/*global define*/
+'use strict';
 
-define([
-  'mvc/Model',
-  'mvc/Collection',
-  'util/Util'
-], function(
-  Model,
-  Collection,
-  Util
-) {
-  'use strict';
+var Collection = require('mvc/Collection'),
+    Model = require('mvc/Model'),
+    Util = require('util/Util');
 
 
-  /** Define default attributes. */
-  var DEFAULTS = {
-    'id': null,
-    'code': null,
-    'name': null,
-    'location': null,
-    'latitude': null,
-    'longitude': null,
-    'geomagnetic_latitude': null,
-    'geomagnetic_longitude': null,
-    'elevation': null,
-    'orientation': null,
-    'instruments': null,
-    'piers': null,
-    'default_pier_id': null
-  };
+var _DEFAULTS = {
+  'id': null,
+  'code': null,
+  'name': null,
+  'location': null,
+  'latitude': null,
+  'longitude': null,
+  'geomagnetic_latitude': null,
+  'geomagnetic_longitude': null,
+  'elevation': null,
+  'orientation': null,
+  'instruments': null,
+  'piers': null,
+  'default_pier_id': null
+};
 
 
-  /**
-   * Constructor.
-   *
-   * @param  options {Object} observatory attributes.
-   */
-  var Observatory = function (options) {
-    // Call parent constructor
-    Model.call(this, Util.extend({}, DEFAULTS, options));
-  };
+/**
+ * Constructor.
+ *
+ * @param  options {Object} observatory attributes.
+ */
+var Observatory = function (options) {
+  var _this,
+      _initialize,
 
-  // Observatory extends Model
-  Observatory.prototype = Object.create(Model.prototype);
+      _options;
 
+  _this = Model(options);
+
+  _options =  Util.extend({}, DEFAULTS, options);
 
   /**
    * Get the default pier for this observatory.
    *
    * @return {Pier} the default pier, or null if no default is specified.
    */
-  Observatory.prototype.getDefaultPier = function () {
+  _this.getDefaultPier = function () {
     var piers = this.get('piers'),
         default_pier_id = this.get('default_pier_id');
     if (piers !== null && default_pier_id !== null) {
@@ -60,13 +53,58 @@ define([
   };
 
   /**
+   * Get "electronics" instruments.
+   *
+   * @return {Collection<Instrument>} instruments with type === 'elec'.
+   */
+  _this.getElectronics = function () {
+    var electronics = [],
+        instruments = this.get('instruments').data(),
+        instrument,
+        i,
+        len;
+    for (i = 0, len = instruments.length; i < len; i++) {
+      instrument = instruments[i];
+      if (instrument.get('type') === 'electronics') {
+        electronics.push(instrument);
+      }
+    }
+    return new Collection(electronics);
+  };
+
+  /**
+   * Get a mark object based on a mark id.
+   *
+   * @param id {Integer}
+   *        the mark id.
+   * @return {Mark} the mark object, or null if not found.
+   */
+  _this.getMarkById = function (id) {
+    var piers = this.get('piers').data(),
+        pier,
+        marks,
+        mark,
+        i,
+        len;
+    for (i = 0, len = piers.length; i < len; i++) {
+      pier = piers[i];
+      marks = pier.get('marks');
+      mark = marks.get(id);
+      if (mark !== null) {
+        return mark;
+      }
+    }
+    return null;
+  };
+
+  /**
    * Get a pier object based on a mark id.
    *
    * @param id {Integer}
    *        the mark id.
    * @return {Pier} the pier object, or null if not found.
    */
-  Observatory.prototype.getPierByMarkId = function (id) {
+  _this.getPierByMarkId = function (id) {
     var piers = this.get('piers').data(),
         pier,
         marks,
@@ -85,58 +123,11 @@ define([
   };
 
   /**
-   * Get a mark object based on a mark id.
-   *
-   * @param id {Integer}
-   *        the mark id.
-   * @return {Mark} the mark object, or null if not found.
-   */
-  Observatory.prototype.getMarkById = function (id) {
-    var piers = this.get('piers').data(),
-        pier,
-        marks,
-        mark,
-        i,
-        len;
-    for (i = 0, len = piers.length; i < len; i++) {
-      pier = piers[i];
-      marks = pier.get('marks');
-      mark = marks.get(id);
-      if (mark !== null) {
-        return mark;
-      }
-    }
-    return null;
-  };
-
-
-  /**
-   * Get "electronics" instruments.
-   *
-   * @return {Collection<Instrument>} instruments with type === 'elec'.
-   */
-  Observatory.prototype.getElectronics = function () {
-    var electronics = [],
-        instruments = this.get('instruments').data(),
-        instrument,
-        i,
-        len;
-    for (i = 0, len = instruments.length; i < len; i++) {
-      instrument = instruments[i];
-      if (instrument.get('type') === 'electronics') {
-        electronics.push(instrument);
-      }
-    }
-    return new Collection(electronics);
-  };
-
-
-  /**
    * Get theodolite instruments.
    *
    * @return {Collection<Instrument>} instruments with type === 'theo'.
    */
-  Observatory.prototype.getTheodolites = function () {
+  _this.getTheodolites = function () {
     var theodolites = [],
         instruments = this.get('instruments').data(),
         instrument,
@@ -151,7 +142,7 @@ define([
     return new Collection(theodolites);
   };
 
+  return _this;
+};
 
-  // return constructor from closure
-  return Observatory;
-});
+module.exports =  Observatory;
