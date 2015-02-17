105e1417b986ca6a45179c2371690e9238646edb
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-17 12:57:17 -0700

    Browserify VerticalIntensitySummaryView

--- src/htdocs/js/geomag/VerticalIntensitySummaryView.js
+++ src/htdocs/js/geomag/VerticalIntensitySummaryView.js
@@ -1,20 +1,10 @@
-/* global define */
-define([
-  'mvc/View',
-  'util/Util',
+'use strict';
 
-  'geomag/Formatter',
-  'geomag/Measurement',
-  'geomag/ObservatoryFactory'
-], function (
-  View,
-  Util,
-
-  Format,
-  Measurement,
-  ObservatoryFactory
-) {
-  'use strict';
+var Format = require('geomag/Formatter'),
+    Measurement = require('geomag/Measurement'),
+    ObservatoryFactory = require('geomag/ObservatoryFactory'),
+    Util = require('util/Util'),
+    View = require('mvc/View');
 
 
   var DEFAULTS = {
@@ -22,47 +12,22 @@ define([
   };
 
 
-  var VerticalIntensitySummaryView = function (options) {
-    this._options = Util.extend({}, DEFAULTS, options);
-    View.call(this, this._options);
-  };
-  VerticalIntensitySummaryView.prototype = Object.create(View.prototype);
+var VerticalIntensitySummaryView = function (options) {
+  var _this,
+      _initialize,
 
+      _options,
 
-  VerticalIntensitySummaryView.prototype.render = function () {
-    var reading = this._reading,
-        measurements = reading.get('measurements').data(),
-        factory = this._options.factory,
-        startTime = null,
-        endTime = null,
-        times;
+      _onChange;
 
-    this._name.innerHTML = reading.get('set_number');
-
-    this._valid.checked = (reading.get('vertical_intensity_valid') === 'Y');
-
-    times = factory.getMeasurementValues(measurements, 'time');
-    if (times.length > 0) {
-      startTime = Math.min.apply(null, times);
-      endTime = Math.max.apply(null, times);
-    }
-    this._startTime.innerHTML = Format.time(startTime);
-    this._endTime.innerHTML = Format.time(endTime);
-
-    this._absValue.innerHTML =
-        Format.nanoteslas(this._calculator.verticalComponent(reading));
-    this._ord.innerHTML = Format.nanoteslas(this._calculator.meanZ(reading));
-    this._baselineValue.innerHTML =
-        Format.nanoteslas(this._calculator.zBaseline(reading));
-    this._observer.innerHTML = this._reading.get('observer') || '';
-  };
-
-  VerticalIntensitySummaryView.prototype._initialize = function () {
-    var el = this._el,
-        factory = this._options.factory,
-        reading = this._options.reading,
+  _initialize = function () {
+    var el = _this._el,
+        factory = _this._options.factory,
+        reading = _this._options.reading,
         i = null,
         len = null;
+
+    _options = Util.extend({}, DEFAULTS, options);
 
     el.innerHTML = [
       '<th class="name" scope="row"></th>',
@@ -75,38 +40,70 @@ define([
       '<td class="observer"></td>'
     ].join('');
 
-    this._name = el.querySelector('.name');
-    this._valid = el.querySelector('.valid > input');
-    this._startTime = el.querySelector('.start-time');
-    this._endTime = el.querySelector('.end-time');
-    this._absValue = el.querySelector('.abs-value');
-    this._ord = el.querySelector('.ord');
-    this._baselineValue = el.querySelector('.baseline-values');
-    this._observer = el.querySelector('.observer');
+    _this._name = el.querySelector('.name');
+    _this._valid = el.querySelector('.valid > input');
+    _this._startTime = el.querySelector('.start-time');
+    _this._endTime = el.querySelector('.end-time');
+    _this._absValue = el.querySelector('.abs-value');
+    _this._ord = el.querySelector('.ord');
+    _this._baselineValue = el.querySelector('.baseline-values');
+    _this._observer = el.querySelector('.observer');
 
-    this._reading = this._options.reading;
-    this._calculator = this._options.calculator;
+    _this._reading = _this._options.reading;
+    _this._calculator = _this._options.calculator;
 
-    this._measurements = factory.getVerticalIntensityMeasurements(reading);
+    _this._measurements = factory.getVerticalIntensityMeasurements(reading);
 
-    this._onChange = this._onChange.bind(this);
-    this._valid.addEventListener('change', this._onChange);
+    _this._valid.addEventListener('change', _onChange);
 
-    this._reading.on('change:vertical_intensity_valid', this.render, this);
+    _this._reading.on('change:vertical_intensity_valid', _this.render, _this);
 
-    this._calculator.on('change', this.render, this);
+    _this._calculator.on('change', _this.render, _this);
 
-    for (i = 0, len = this._measurements.length; i < len; i++) {
-      this._measurements[i].on('change', this.render, this);
+    for (i = 0, len = _this._measurements.length; i < len; i++) {
+      _this._measurements[i].on('change', _this.render, _this);
     }
-    this.render();
+    _this.render();
   };
 
-  VerticalIntensitySummaryView.prototype._onChange = function () {
-    this._reading.set({
-      vertical_intensity_valid: (this._valid.checked ? 'Y' : 'N')
+  _onChange = function () {
+    _this._reading.set({
+      vertical_intensity_valid: (_this._valid.checked ? 'Y' : 'N')
     });
   };
 
-  return VerticalIntensitySummaryView;
-});
+
+  _this.render = function () {
+    var reading = _this._reading,
+        measurements = reading.get('measurements').data(),
+        factory = _this._options.factory,
+        startTime = null,
+        endTime = null,
+        times;
+
+    _this._name.innerHTML = reading.get('set_number');
+
+    _this._valid.checked = (reading.get('vertical_intensity_valid') === 'Y');
+
+    times = factory.getMeasurementValues(measurements, 'time');
+    if (times.length > 0) {
+      startTime = Math.min.apply(null, times);
+      endTime = Math.max.apply(null, times);
+    }
+    _this._startTime.innerHTML = Format.time(startTime);
+    _this._endTime.innerHTML = Format.time(endTime);
+
+    _this._absValue.innerHTML =
+        Format.nanoteslas(_this._calculator.verticalComponent(reading));
+    _this._ord.innerHTML = Format.nanoteslas(_this._calculator.meanZ(reading));
+    _this._baselineValue.innerHTML =
+        Format.nanoteslas(_this._calculator.zBaseline(reading));
+    _this._observer.innerHTML = _this._reading.get('observer') || '';
+  };
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = VerticalIntensitySummaryView;
