092bc6f55bb93f4ff49ff1f221b975a8385a157b
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-17 13:06:53 -0700

    Remove references to baseline calculator folder.

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -4,36 +4,36 @@ var config = require('./config');
 
 // Modules defined by this (geomag-baseline-calculator) package
 var EXPORTS = [
-  'baselinecalculator/htdocs/js/geomag/BaselineCalculator',
-  'baselinecalculator/htdocs/js/geomag/DeclinationSummaryView',
-  'baselinecalculator/htdocs/js/geomag/DeclinationView',
-  'baselinecalculator/htdocs/js/geomag/Formatter',
-  'baselinecalculator/htdocs/js/geomag/HorizontalIntensitySummaryView',
-  'baselinecalculator/htdocs/js/geomag/InclinationView',
-  'baselinecalculator/htdocs/js/geomag/Instrument',
-  'baselinecalculator/htdocs/js/geomag/MagnetometerOrdinatesView',
-  'baselinecalculator/htdocs/js/geomag/Mark',
-  'baselinecalculator/htdocs/js/geomag/Measurement',
-  'baselinecalculator/htdocs/js/geomag/MeasurementView',
-  'baselinecalculator/htdocs/js/geomag/Observation',
-  'baselinecalculator/htdocs/js/geomag/ObservationBaselineCalculator',
-  'baselinecalculator/htdocs/js/geomag/ObservationMetaView',
-  'baselinecalculator/htdocs/js/geomag/ObservationSummaryView',
-  'baselinecalculator/htdocs/js/geomag/ObservationsView',
-  'baselinecalculator/htdocs/js/geomag/ObservationView',
-  'baselinecalculator/htdocs/js/geomag/Pier',
-  'baselinecalculator/htdocs/js/geomag/Reading',
-  'baselinecalculator/htdocs/js/geomag/ReadingGroupView',
-  'baselinecalculator/htdocs/js/geomag/ReadingView',
-  'baselinecalculator/htdocs/js/geomag/RealtimeData',
-  'baselinecalculator/htdocs/js/geomag/RealtimeDataFactory',
-  'baselinecalculator/htdocs/js/geomag/User',
-  'baselinecalculator/htdocs/js/geomag/UserAdminView',
-  'baselinecalculator/htdocs/js/geomag/UserEditView',
-  'baselinecalculator/htdocs/js/geomag/UserFactory',
-  'baselinecalculator/htdocs/js/geomag/UsersView',
-  'baselinecalculator/htdocs/js/geomag/Validate',
-  'baselinecalculator/htdocs/js/geomag/VerticalIntensitySummaryView'
+  'geomag/BaselineCalculator',
+  'geomag/DeclinationSummaryView',
+  'geomag/DeclinationView',
+  'geomag/Formatter',
+  'geomag/HorizontalIntensitySummaryView',
+  'geomag/InclinationView',
+  'geomag/Instrument',
+  'geomag/MagnetometerOrdinatesView',
+  'geomag/Mark',
+  'geomag/Measurement',
+  'geomag/MeasurementView',
+  'geomag/Observation',
+  'geomag/ObservationBaselineCalculator',
+  'geomag/ObservationMetaView',
+  'geomag/ObservationSummaryView',
+  'geomag/ObservationsView',
+  'geomag/ObservationView',
+  'geomag/Pier',
+  'geomag/Reading',
+  'geomag/ReadingGroupView',
+  'geomag/ReadingView',
+  'geomag/RealtimeData',
+  'geomag/RealtimeDataFactory',
+  'geomag/User',
+  'geomag/UserAdminView',
+  'geomag/UserEditView',
+  'geomag/UserFactory',
+  'geomag/UsersView',
+  'geomag/Validate',
+  'geomag/VerticalIntensitySummaryView'
 ];
 
 
@@ -42,19 +42,34 @@ var browserify = {
     browserifyOptions: {
       debug: true,
       paths: [
-        process.cwd() + '/' + config.src,
-        process.cwd() + '/node_modules/hazdev-tablist/src/tablist',
+        process.cwd() + '/' + config.src + '/htdocs/js',
+        process.cwd() + '/node_modules/hazdev-tablist/src',
         process.cwd() + '/node_modules/hazdev-webutils/src'
       ]
     }
   },
 
+  index: {
+    src: [config.src + '/htdocs/js/index.js'],
+    dest: config.build + '/' + config.src + '/htdocs/js/index.js'
+  },
+
+  observation: {
+    src: [config.src + '/htdocs/js/observation.js'],
+    dest: config.build + '/' + config.src + '/htdocs/js/observation.js'
+  },
+
+  useradmin: {
+    src: [config.src + '/htdocs/js/useradmin.js'],
+    dest: config.build + '/' + config.src + '/htdocs/js/useradmin.js'
+  },
+
   source: {
     src: [],
-    dest: config.build + '/' + config.src + '/geomag-baseline-calculator.js',
+    dest: config.build + '/' + config.test + '/geomag-baseline-calculator.js',
     options: {
       alias: EXPORTS.map(function (path) {
-        return './' + config.src + '/' + path + '.js:' + path;
+        return './' + config.src + '/htdocs/js/' + path + '.js:' + path;
       })
     }
   },
