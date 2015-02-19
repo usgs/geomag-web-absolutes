8fdce56d79972d22eed0ce3ebafc59eec884d9c9
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-19 10:00:45 -0700

    Replaced tabs with spaces

--- src/htdocs/js/geomag/Validate.js
+++ src/htdocs/js/geomag/Validate.js
@@ -4,55 +4,55 @@ define([
 ) {
 'use strict';
 
-	var Validate = {
+  var Validate = {
 
-		validDegrees: function (value) {
-			if(value === null || value === '') {
-				return false;
-			}
-			return this.inRange(value, 0, 360);
-		},
+    validDegrees: function (value) {
+      if(value === null || value === '') {
+        return false;
+      }
+      return this.inRange(value, 0, 360);
+    },
 
-		validSeconds: function (value) {
-			if(value === null || value === '') {
-				return false;
-			}
-			return this.inRange(value, 0, 60);
-		},
+    validSeconds: function (value) {
+      if(value === null || value === '') {
+        return false;
+      }
+      return this.inRange(value, 0, 60);
+    },
 
-		validMinutes: function (value) {
-			if(value === null || value === '') {
-				return false;
-			}
-			return this.inRange(value, 0, 60);
-		},
+    validMinutes: function (value) {
+      if(value === null || value === '') {
+        return false;
+      }
+      return this.inRange(value, 0, 60);
+    },
 
-		validTime: function (value) {
-			// for 24-hour time, leading zeroes mandatory.
-			var pattern = /^(2[0-3]|[01][0-9]):?[0-5][0-9]:?[0-5][0-9]$/;
-			if (value.match(pattern)) {
-				return true;
-			}
-			return false;
-		},
+    validTime: function (value) {
+      // for 24-hour time, leading zeroes mandatory.
+      var pattern = /^(2[0-3]|[01][0-9]):?[0-5][0-9]:?[0-5][0-9]$/;
+      if (value.match(pattern)) {
+        return true;
+      }
+      return false;
+    },
 
-		isNull: function (value) {
-			if (!value) {
-				return true;
-			}
-		},
+    isNull: function (value) {
+      if (!value) {
+        return true;
+      }
+    },
 
-		inRange: function (value, begin, end) {
-			if (isNaN(value)) {
-				return false;
-			}
-			if (value >= begin && value < end) {
-				return true;
-			}
-			return false;
-		}
+    inRange: function (value, begin, end) {
+      if (isNaN(value)) {
+        return false;
+      }
+      if (value >= begin && value < end) {
+        return true;
+      }
+      return false;
+    }
 
-	};
+  };
 
-	return Validate;
+  return Validate;
 });
