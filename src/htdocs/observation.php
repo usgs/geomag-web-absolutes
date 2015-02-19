36a9b823ddbd3acf6a0fd527f9ee4364fd004056
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-19 09:18:31 -0700

    changed tabs to spaces

--- src/htdocs/observation.php
+++ src/htdocs/observation.php
@@ -1,29 +1,29 @@
 <?php
 if (!isset($TEMPLATE)) {
-	include_once '../conf/config.inc.php';
-	include_once 'functions.inc.php';
+  include_once '../conf/config.inc.php';
+  include_once 'functions.inc.php';
 
-	$id = param('id', 'null');
-	if ($id !== 'null') {
-		$id = intval($id, 10);
-	}
+  $id = param('id', 'null');
+  if ($id !== 'null') {
+    $id = intval($id, 10);
+  }
 
-	$TITLE = 'Observation Input';
+  $TITLE = 'Observation Input';
 
-	$NAVIGATION = true;
-	$HEAD = '<link rel="stylesheet" href="'.$MOUNT_PATH.'/css/observation.css"/><script src="requirejs/require.js"></script>';
-	$FOOT =
-		'
-		<script>
-			var observationId = ' . $id . ';
-			var MOUNT_PATH = \'' . $MOUNT_PATH . '\';
-			var REALTIME_DATA_URL = \'' . $REALTIME_DATA_URL . '\';
-		</script>' .
-		'<script src="' . $MOUNT_PATH . '/js/observation.js"></script>' .
-		'<script src="http://localhost:35729/livereload.js?snipver=1"></script>';
+  $NAVIGATION = true;
+  $HEAD = '<link rel="stylesheet" href="'.$MOUNT_PATH.'/css/observation.css"/><script src="requirejs/require.js"></script>';
+  $FOOT =
+    '
+    <script>
+      var observationId = ' . $id . ';
+      var MOUNT_PATH = \'' . $MOUNT_PATH . '\';
+      var REALTIME_DATA_URL = \'' . $REALTIME_DATA_URL . '\';
+    </script>' .
+    '<script src="' . $MOUNT_PATH . '/js/observation.js"></script>' .
+    '<script src="http://localhost:35729/livereload.js?snipver=1"></script>';
 
-	include '../lib/login.inc.php';
-	include 'template.inc.php';
+  include '../lib/login.inc.php';
+  include 'template.inc.php';
 }
 ?>
 
