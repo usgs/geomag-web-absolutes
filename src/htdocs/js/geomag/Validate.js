d271a192f28e375478ae5ba606740e6b80bd0441
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-19 09:08:52 -0700

    Browserify Validate

--- src/htdocs/js/geomag/Validate.js
+++ src/htdocs/js/geomag/Validate.js
@@ -1,58 +1,53 @@
-/* global define */
-define([
-], function (
-) {
 'use strict';
 
-  var Validate = {
+var Validate = {
 
-    validDegrees: function (value) {
-      if(value === null || value === '') {
-        return false;
-      }
-      return this.inRange(value, 0, 360);
-    },
-
-    validSeconds: function (value) {
-      if(value === null || value === '') {
-        return false;
-      }
-      return this.inRange(value, 0, 60);
-    },
-
-    validMinutes: function (value) {
-      if(value === null || value === '') {
-        return false;
-      }
-      return this.inRange(value, 0, 60);
-    },
-
-    validTime: function (value) {
-      // for 24-hour time, leading zeroes mandatory.
-      var pattern = /^(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/;
-      if (value.match(pattern)) {
-        return true;
-      }
-      return false;
-    },
-
-    isNull: function (value) {
-      if (!value) {
-        return true;
-      }
-    },
-
-    inRange: function (value, begin, end) {
-      if (isNaN(value)) {
-        return false;
-      }
-      if (value >= begin && value < end) {
-        return true;
-      }
+  validDegrees: function (value) {
+    if(value === null || value === '') {
       return false;
     }
+    return this.inRange(value, 0, 360);
+  },
 
-  };
+  validSeconds: function (value) {
+    if(value === null || value === '') {
+      return false;
+    }
+    return this.inRange(value, 0, 60);
+  },
 
-  return Validate;
-});
+  validMinutes: function (value) {
+    if(value === null || value === '') {
+      return false;
+    }
+    return this.inRange(value, 0, 60);
+  },
+
+  validTime: function (value) {
+    // for 24-hour time, leading zeroes mandatory.
+    var pattern = /^(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/;
+    if (value.match(pattern)) {
+      return true;
+    }
+    return false;
+  },
+
+  isNull: function (value) {
+    if (!value) {
+      return true;
+    }
+  },
+
+  inRange: function (value, begin, end) {
+    if (isNaN(value)) {
+      return false;
+    }
+    if (value >= begin && value < end) {
+      return true;
+    }
+    return false;
+  }
+
+};
+
+module.exports = Validate;
