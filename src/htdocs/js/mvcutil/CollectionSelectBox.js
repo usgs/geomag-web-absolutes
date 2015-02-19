5489a38aafbdbe0ea90d0506a9fc21a03f102600
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-19 09:09:09 -0700

    Browserify CollectionSelectBox.

--- src/htdocs/js/mvcutil/CollectionSelectBox.js
+++ src/htdocs/js/mvcutil/CollectionSelectBox.js
@@ -1,145 +1,117 @@
-/* global define */
-define([
-  'util/Util',
-  'mvc/View'
-], function (
-  Util,
-  View
-) {
-  'use strict';
+'use strict';
+
+var Util = require('util/Util'),
+    View = require('mvc/View');
 
 
-  // default options
-  var DEFAULTS = {
-    allowDeselect: false,
-    emptyText: '',
-    collection: null,
-    formatOption: function (model) {
-      return model.get('name');
-    }
-  };
+var DEFAULTS = {
+  allowDeselect: false,
+  emptyText: '',
+  collection: null,
+  formatOption: function (model) {
+    return model.get('name');
+  }
+};
 
 
-  /**
-   * Create a new CollectionSelectBox.
-   *
-   * @param options {Object}
-   * @param options.el {SelectElement}
-   *        "select" element to manage.
-   * @param options.allowDeselect {Boolean}
-   *        add an empty option used to trigger collection deselect.
-   *        default false.
-   * @param emptyText {String}
-   *        text to display when collection is null or empty.
-   *        default ''.
-   * @param options.collection {Collection}
-   *        collection to manage.
-   *        Optional, can be changed later using setCollection().
-   * @param options.formatOption {Function(model)}
-   *        format method for option content.
-   *        default returns model.get('name');
-   */
-  var CollectionSelectBox = function (options) {
-    this._options = Util.extend({}, DEFAULTS, options);
-    View.call(this, this._options);
-  };
-  // CollectionSelectBox is a View
-  CollectionSelectBox.prototype = Object.create(View.prototype);
+/**
+ * Create a new CollectionSelectBox.
+ *
+ * @param options {Object}
+ * @param options.el {SelectElement}
+ *        "select" element to manage.
+ * @param options.allowDeselect {Boolean}
+ *        add an empty option used to trigger collection deselect.
+ *        default false.
+ * @param emptyText {String}
+ *        text to display when collection is null or empty.
+ *        default ''.
+ * @param options.collection {Collection}
+ *        collection to manage.
+ *        Optional, can be changed later using setCollection().
+ * @param options.formatOption {Function(model)}
+ *        format method for option content.
+ *        default returns model.get('name');
+ */
+var CollectionSelectBox = function (options) {
+  var _this,
+      _initialize,
 
+      _options;
 
+    _this = View(options);
   /**
    * Initialize the CollectionSelectBox.
    */
-  CollectionSelectBox.prototype._initialize = function () {
-    // bind callbacks
-    this.render = this.render.bind(this);
-    this._onChange = this._onChange.bind(this);
-    this._onSelect = this._onSelect.bind(this);
+  _initialize = function () {
+    _options = Util.extend({}, DEFAULTS, options);
+
     // add select box change listener
-    this._el.addEventListener('change', this._onChange);
+    _this.el.addEventListener('change', _onChange);
     // initialize collection
-    this._collection = null;
-    if (this._options.collection !== null) {
-      this.setCollection(this._options.collection);
+    _this._collection = null;
+    if (_options.collection !== null) {
+      _this.setCollection(_options.collection);
     }
   };
 
   /**
-   * Select an item in the collection based on its id.
-   *
-   * @param id to select.
+   * SelectBox "change" event handler.
    */
-  CollectionSelectBox.prototype.selectById = function (id) {
-    var collection = this._collection,
-        item = null;
-    if (collection !== null) {
-      if (id !== null) {
-        item = collection.get(id);
-      }
-      if (item !== null) {
-        collection.select(item);
-      } else {
-        collection.deselect();
-      }
+  _onChange = function () {
+    var value = _this.el.value,
+        collection = _this._collection,
+        selected = null;
+    if (value === '') {
+      collection.deselect();
+    } else {
+      selected = collection.get(parseInt(value, 10));
+      collection.select(selected);
     }
+  };
+
+  /**
+   * Collection "select" event handler.
+   */
+  _onSelect = function () {
+    var el = _this.el,
+        oldValue = el.value,
+        newValue = '',
+        collection = _this._collection,
+        selected = collection.getSelected();
+    if (selected !== null) {
+      newValue = selected.id;
+    }
+    if (oldValue !== newValue) {
+      el.value = newValue;
+    }
+    _this.trigger('change', selected, collection);
+  };
+
+
+  /**
+   * Add "disabled" attribute to element.
+   */
+  _this.disable = function () {
+    this._el.setAttribute('disabled', 'disabled');
   };
 
   /**
    * Remove "disabled" attribute from element.
    */
-  CollectionSelectBox.prototype.enable = function () {
-    this._el.removeAttribute('disabled');
-  };
-
-  /**
-   * Add "disabled" attribute to element.
-   */
-  CollectionSelectBox.prototype.disable = function () {
-    this._el.setAttribute('disabled', 'disabled');
-  };
-
-  /**
-   * Set the collection to display.
-   * Calls render after binding events.
-   *
-   * @param collection {Collection}
-   *        a collection with items to display.
-   */
-  CollectionSelectBox.prototype.setCollection = function (collection) {
-    var render = this.render,
-        select = this._onSelect,
-        oldCollection;
-    // remove existing bindings
-    oldCollection = this._collection;
-    if (oldCollection !== null) {
-      oldCollection.off('add', render, this);
-      oldCollection.off('remove', render, this);
-      oldCollection.off('reset', render, this);
-      oldCollection.off('select', select, this);
-      oldCollection = null;
-    }
-    // add new bindings
-    this._collection = collection;
-    if (collection !== null) {
-      collection.on('add', render, this);
-      collection.on('remove', render, this);
-      collection.on('reset', render, this);
-      collection.on('select', select, this);
-    }
-    // redraw
-    this.render();
+  _this.enable = function () {
+    _this.el.removeAttribute('disabled');
   };
 
   /**
    * Render select box content.
    */
-  CollectionSelectBox.prototype.render = function () {
-    var el = this._el,
-        options = this._options,
-        allowDeselect = options.allowDeselect,
-        emptyText = options.emptyText,
-        formatOption = options.formatOption,
-        collection = this._collection,
+  _this.render = function () {
+    var el = _this.el,
+        allowDeselect = _options.allowDeselect,
+        emptyText = _options.emptyText,
+        formatOption = _options.formatOption,
+        collection = _this._collection,
         data,
         selected = null,
         buf,
@@ -154,7 +126,7 @@ define([
       // clear element
       el.innerHTML = emptyText;
       el.value = '';
-      this.disable();
+      _this.disable();
     } else {
       buf = [];
       data = collection.data();
@@ -171,46 +143,67 @@ define([
       // update element
       el.innerHTML = buf.join('');
       el.value = (selected === null ? '' : selected.id);
-      this.enable();
+      _this.enable();
     }
 
-    this.trigger('change', selected, collection);
-  };
-
-
-  /**
-   * SelectBox "change" event handler.
-   */
-  CollectionSelectBox.prototype._onChange = function () {
-    var value = this._el.value,
-        collection = this._collection,
-        selected = null;
-    if (value === '') {
-      collection.deselect();
-    } else {
-      selected = collection.get(parseInt(value, 10));
-      collection.select(selected);
-    }
+    _this.trigger('change', selected, collection);
   };
 
   /**
-   * Collection "select" event handler.
+   * Select an item in the collection based on its id.
+   *
+   * @param id to select.
    */
-  CollectionSelectBox.prototype._onSelect = function () {
-    var el = this._el,
-        oldValue = el.value,
-        newValue = '',
-        collection = this._collection,
-        selected = collection.getSelected();
-    if (selected !== null) {
-      newValue = selected.id;
+  _this.selectById = function (id) {
+    var collection = _this._collection,
+        item = null;
+    if (collection !== null) {
+      if (id !== null) {
+        item = collection.get(id);
+      }
+      if (item !== null) {
+        collection.select(item);
+      } else {
+        collection.deselect();
+      }
     }
-    if (oldValue !== newValue) {
-      el.value = newValue;
-    }
-    this.trigger('change', selected, collection);
   };
 
+  /**
+   * Set the collection to display.
+   * Calls render after binding events.
+   *
+   * @param collection {Collection}
+   *        a collection with items to display.
+   */
+  _this.setCollection = function (collection) {
+    var select = _onSelect,
+        oldCollection;
 
-  return CollectionSelectBox;
-});
+    // remove existing bindings
+    oldCollection = _this._collection;
+    if (oldCollection !== null) {
+      oldCollection.off('add', _this.render, _this);
+      oldCollection.off('remove', _this.render, _this);
+      oldCollection.off('reset', _this.render, _this);
+      oldCollection.off('select', select, _this);
+      oldCollection = null;
+    }
+    // add new bindings
+    _this._collection = collection;
+    if (collection !== null) {
+      collection.on('add', _this.render, _this);
+      collection.on('remove', _this.render, _this);
+      collection.on('reset', _this.render, _this);
+      collection.on('select', _this.select, _this);
+    }
+    // redraw
+    _this.render();
+  };
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+module.exports = CollectionSelectBox;
--- test/spec/mvcutil/CollectionSelectBoxTest.js
+++ test/spec/mvcutil/CollectionSelectBoxTest.js
@@ -1,120 +1,109 @@
-/* global define, describe, it */
-define([
-  'chai',
+/* global chai, describe, it */
+'use strict';
 
-  'mvc/Collection',
-  'mvc/Model',
-  'mvcutil/CollectionSelectBox'
-], function (
-  chai,
-
-  Collection,
-  Model,
-  CollectionSelectBox
-) {
-  'use strict';
+var Collection = require('mvc/Collection'),
+    CollectionSelectBox = require('mvcutil/CollectionSelectBox'),
+    Model = require('mvc/Model');
 
 
-  var expect = chai.expect;
+var expect = chai.expect;
 
-  // collection object for testing.
-  var COLLECTION = new Collection([
-    new Model({id: 11, name: 'test 11'}),
-    new Model({id:  5, name: 'test 5'}),
-    new Model({id: 17, name: 'test 17'})
-  ]);
+// collection object for testing.
+var COLLECTION = new Collection([
+  new Model({id: 11, name: 'test 11'}),
+  new Model({id:  5, name: 'test 5'}),
+  new Model({id: 17, name: 'test 17'})
+]);
 
-  var SELECT_EL = document.createElement('select');
+var SELECT_EL = document.createElement('select');
 
 
-  describe('Unit tests for CollectionSelectBox', function () {
+describe('Unit tests for CollectionSelectBox', function () {
 
-    var selectBox = new CollectionSelectBox({
-      el: SELECT_EL,
-      collection: COLLECTION
+  var selectBox = new CollectionSelectBox({
+    el: SELECT_EL,
+    collection: COLLECTION
+  });
+
+  describe('when initialized', function () {
+
+    it('is deselected by default', function () {
+      expect(SELECT_EL.value).to.equal('');
     });
 
-    describe('when initialized', function () {
-
-      it('is deselected by default', function () {
-        expect(SELECT_EL.value).to.equal('');
-      });
-
-      it('has one option for each item', function () {
-        // plus one for default item
-        expect(SELECT_EL.childNodes.length).to.equal(
-          COLLECTION.data().length + 1);
-      });
-
-      it ('preserves collection order', function () {
-        for (var i = 0, len = SELECT_EL.childNodes.length - 1; i < len; i++) {
-          expect(parseInt(SELECT_EL.childNodes[i+1].value, 10)).to.equal(
-              COLLECTION.data()[i].id);
-        }
-      });
-
+    it('has one option for each item', function () {
+      // plus one for default item
+      expect(SELECT_EL.childNodes.length).to.equal(
+        COLLECTION.data().length + 1);
     });
 
-    describe('option values', function () {
-
-      it('use formatOption method', function () {
-        var oldFormat = selectBox._options.formatOption;
-        selectBox._options.formatOption = function (obj) {
-          return 'blah' + obj.id;
-        };
-
-        // re-render
-        selectBox.render();
-        // check
-        expect(SELECT_EL.childNodes[1].textContent).to.equal('blah11');
-
-        // restore
-        selectBox._options.formatOption = oldFormat;
-        selectBox.render();
-      });
-
+    it ('preserves collection order', function () {
+      for (var i = 0, len = SELECT_EL.childNodes.length - 1; i < len; i++) {
+        expect(parseInt(SELECT_EL.childNodes[i+1].value, 10)).to.equal(
+            COLLECTION.data()[i].id);
+      }
     });
 
+  });
 
-    describe('when collection is changed', function () {
+  describe('option values', function () {
 
-      it('triggers change event', function (done) {
-        var onChange = function () {
-          selectBox.off('change', onChange);
-          done();
-        };
+    it('use formatOption method', function () {
+      var oldFormat = selectBox._options.formatOption;
+      selectBox._options.formatOption = function (obj) {
+        return 'blah' + obj.id;
+      };
 
-        selectBox.on('change', onChange);
-        COLLECTION.add(new Model({id: 1, name: 'test 1'}));
-      });
+      // re-render
+      selectBox.render();
+      // check
+      expect(SELECT_EL.childNodes[1].textContent).to.equal('blah11');
 
+      // restore
+      selectBox._options.formatOption = oldFormat;
+      selectBox.render();
     });
 
-    describe('when collection is selected', function () {
+  });
 
-      it('triggers change event', function (done) {
-        var onChange = function () {
-          selectBox.off('change', onChange);
-          done();
-        };
 
-        COLLECTION.select(COLLECTION.get(11));
-        selectBox.on('change', onChange);
-        COLLECTION.select(COLLECTION.get(5));
-      });
+  describe('when collection is changed', function () {
 
-      it('selectbox value updates', function () {
-        // set collection to 11
-        COLLECTION.select(COLLECTION.get(11));
-        expect(COLLECTION.getSelected().id).to.equal(11);
-        expect(parseInt(SELECT_EL.value, 10)).to.equal(11);
+    it('triggers change event', function (done) {
+      var onChange = function () {
+        selectBox.off('change', onChange);
+        done();
+      };
 
-        // now select by id
-        selectBox.selectById(5);
-        expect(COLLECTION.getSelected().id).to.equal(5);
-        expect(parseInt(SELECT_EL.value, 10)).to.equal(5);
-      });
+      selectBox.on('change', onChange);
+      COLLECTION.add(new Model({id: 1, name: 'test 1'}));
+    });
 
+  });
+
+  describe('when collection is selected', function () {
+
+    it('triggers change event', function (done) {
+      var onChange = function () {
+        selectBox.off('change', onChange);
+        done();
+      };
+
+      COLLECTION.select(COLLECTION.get(11));
+      selectBox.on('change', onChange);
+      COLLECTION.select(COLLECTION.get(5));
+    });
+
+    it('selectbox value updates', function () {
+      // set collection to 11
+      COLLECTION.select(COLLECTION.get(11));
+      expect(COLLECTION.getSelected().id).to.equal(11);
+      expect(parseInt(SELECT_EL.value, 10)).to.equal(11);
+
+      // now select by id
+      selectBox.selectById(5);
+      expect(COLLECTION.getSelected().id).to.equal(5);
+      expect(parseInt(SELECT_EL.value, 10)).to.equal(5);
     });
 
   });
