abe499d225b45140a31a79041fd5a81813c25b36
Eric Martinez <emartinez@usgs.gov>
2015-02-23 12:34:07 -0700

    Made getCurrentUser a real static method.

--- src/htdocs/js/geomag/User.js
+++ src/htdocs/js/geomag/User.js
@@ -24,27 +24,25 @@ var _DEFAULTS = {
  * Construct a new User object.
  */
 var User = function (options) {
-  var _this,
-      _initialize;
+  var _this;
 
   _this = Model(Util.extend({}, _DEFAULTS, options));
 
-  _initialize = function () {};
-  /**
-   * Static access to current user object.
-   *
-   * @return currently logged in user.
-   */
-  _this.getCurrentUser = function () {
-    if (_CURRENT_USER === null) {
-      _CURRENT_USER = User(CurrentUser);
-    }
-    return _CURRENT_USER;
-  };
-
-  _initialize();
   options = null;
   return _this;
+};
+
+/**
+ * Static access to current user object.
+ *
+ * @return currently logged in user.
+ */
+User.getCurrentUser = function () {
+  if (_CURRENT_USER === null) {
+    _CURRENT_USER = User(CurrentUser);
+  }
+
+  return _CURRENT_USER;
 };
 
 module.exports = User;
