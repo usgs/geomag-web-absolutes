812afe1ab2ade35c41c74b772119405a618af9ab
Eric Martinez <emartinez@usgs.gov>
2015-02-24 12:32:07 -0700

    Scaffold for BaselinePlot class.

--- src/htdocs/js/plot.js
+++ src/htdocs/js/plot.js
@@ -1,2 +1,52 @@
-/* g lobal observationId, MOUNT_PATH */
+/* global observatoryId */
 'use strict';
+
+var BaselinePlot = require('geomag/BaselinePlot'),
+    Collection = require('mvc/Collection'),
+    CollectionSelectBox = require('mvc/CollectionSelectBox'),
+    ObservatoryFactory = require('geomag/ObservatoryFactory'),
+    User = require('geomag/User');
+
+
+var currentUser = User.getCurrentUser(),
+    factory = ObservatoryFactory(),
+    observatories = Collection([]);
+
+observatories.on('select', function () {
+  window.location.hash = observatories.getSelected().id;
+});
+
+factory.getObservatories({
+  success: function (data) {
+    var defaultId;
+
+    observatories.reset(data);
+
+    if (observatoryId !== null) {
+      defaultId = observatoryId;
+    } else if (window.location.hash) {
+      defaultId = parseInt(window.location.hash.substring(1), 10);
+    } else if (currentUser && currentUser.get('default_observatory_id')) {
+      defaultId = currentUser.get('default_observatory_id');
+    } else {
+      defaultId = data[0].id;
+    }
+
+    observatories.selectById(defaultId);
+  },
+  error: function () {
+    console.log('Failed to get observatories...');
+  }
+});
+
+CollectionSelectBox({
+  el: document.querySelector('.observatory-select-box'),
+  collection: observatories,
+  format: function (obs) {
+    return obs.get('name');
+  }
+});
+
+BaselinePlot({
+  el: document.querySelector('.observation-baseline-plot')
+});
