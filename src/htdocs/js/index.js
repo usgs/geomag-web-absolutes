63ce131e469f4aa2b6c0b94c12d658dcc3b3bcb2
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-26 13:13:53 -0700

    Default observatory works on dashboard for non-admin users.

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -5,8 +5,7 @@ var ObservatoryView = require('geomag/ObservatoryView'),
     User = require('geomag/User');
 
 var id = observatoryId,
-    // user = User.getCurrentUser();
-    user = User();
+    user = User.getCurrentUser();
 
 if (user.get('admin') !== 'Y') {
   id = parseInt(user.get('default_observatory_id'), 10);
