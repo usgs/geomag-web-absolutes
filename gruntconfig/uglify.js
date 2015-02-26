b509459a37ac09885bf3d4d84a3bdf057042a79b
Eric Martinez <emartinez@usgs.gov>
2015-02-26 13:40:57 -0700

    Created uglify targets for each bundle.

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -3,9 +3,21 @@
 var config = require('./config');
 
 var uglify = {
-  dist: {
-    src: config.build + '/' + config.src + '/geomag-baseline-calculator.js',
-    dest: config.dist + '/geomag-baseline-calculator.js'
+  index: {
+    src: [config.build + '/' + config.src + '/htdocs/js/index.js'],
+    dest: config.dist + '/htdocs/js/index.js',
+  },
+  observation: {
+    src: [config.build + '/' + config.src + '/htdocs/js/observation.js'],
+    dest: config.dist + '/htdocs/js/observation.js'
+  },
+  plot: {
+    src: [config.build + '/' + config.src + '/htdocs/js/plot.js'],
+    dest: config.dist + '/htdocs/js/plot.js'
+  },
+  useradmin: {
+    src: [config.build + '/' + config.src + '/htdocs/js/useradmin.js'],
+    dest: config.dist + '/htdocs/js/useradmin.js'
   }
 };
 
