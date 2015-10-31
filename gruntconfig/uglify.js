6400edf3ac50bcaaf59c4daefa11f605094b29d4
Eddie McWhirter <emcwhirter@usgs.gov>
2015-10-30 20:47:07 -0600

    Alphabetize and add spacing to ugilfy.js.

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -5,23 +5,31 @@ var config = require('./config');
 
 var uglify = {
   index: {
-    src: [config.build + '/' + config.src + '/htdocs/js/index.js'],
     dest: config.dist + '/htdocs/js/index.js',
+    src: [
+      config.build + '/' + config.src + '/htdocs/js/index.js'
+    ]
   },
 
   observation: {
-    src: [config.build + '/' + config.src + '/htdocs/js/observation.js'],
-    dest: config.dist + '/htdocs/js/observation.js'
+    dest: config.dist + '/htdocs/js/observation.js',
+    src: [
+      config.build + '/' + config.src + '/htdocs/js/observation.js'
+    ]
   },
 
   plot: {
-    src: [config.build + '/' + config.src + '/htdocs/js/plot.js'],
-    dest: config.dist + '/htdocs/js/plot.js'
+    dest: config.dist + '/htdocs/js/plot.js',
+    src: [
+      config.build + '/' + config.src + '/htdocs/js/plot.js'
+    ]
   },
-  
+
   useradmin: {
-    src: [config.build + '/' + config.src + '/htdocs/js/useradmin.js'],
-    dest: config.dist + '/htdocs/js/useradmin.js'
+    dest: config.dist + '/htdocs/js/useradmin.js',
+    src: [
+      config.build + '/' + config.src + '/htdocs/js/useradmin.js'
+    ]
   }
 };
 
