7a8a6c7e255a4f6d990a11b268f7b241cdc7878f
Jeremy Fee <jmfee@usgs.gov>
2018-08-24 10:29:57 -0600

    Configure session lifetime to be 8 hours

--- src/lib/login.inc.php
+++ src/lib/login.inc.php
@@ -5,6 +5,11 @@ include_once('../conf/config.inc.php');
 
 // pages using authentication shouldn't be cached.
 header('Cache-Control:no-cache, no-store');
+
+// keep inactive sessions for 8 hours
+ini_set("session.gc_maxlifetime", "28800");
+// keep cookie until browser closes
+ini_set("session.cookie_lifetime", "0");
 session_start();
 
 // load user from session.
